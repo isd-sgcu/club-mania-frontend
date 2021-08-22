5f23f8a44122637df163f7d67a6c6393fa18e899
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-22 21:09:20 +0700

    🎉 cleaned template's codes

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import { useHead } from '@vueuse/head'
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Club Mania',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Club Mania description' },
   ],
 })
 </script>
