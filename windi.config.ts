a24d40c73b24b058cbb64ad6fd2c946731deaffd
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-22 22:54:24 +0700

    💄 config ui แต่มึนๆ อยู่

--- windi.config.ts
+++ windi.config.ts
@@ -12,6 +12,26 @@ export default defineConfig({
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Mitr: ["Mitr", "Arial", "sans"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        red: "#DF322A",
+        navy: "##0C233F",
+        streetLight: "##8DB4DA",
+        purple: {
+          400: "#6E459B",
+          500: "#633E8B",
+          600: "#5C3A87",
+          700: "#52317C",
+          900: "#391E67",
+          "900p": "#240F46",
+          "900++": "#1B0D31",
+          "a400": "#7427D2",
+          "textBox": "#3F2964",
+        },
+      },
       typography: {
         DEFAULT: {
           css: {
