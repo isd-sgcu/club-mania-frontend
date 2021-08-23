2536e0cb776256dbf6102f249ee344bef17ccd84
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-23 21:52:02 +0700

    🎨Style Navbar (search function isn't working yet)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="headbar container h-64px backdrop-blur-14px overflow-hidden flex">
-    <h2>Club Mania</h2>
+  <div
+    class="headbar container h-64px backdrop-blur-14px overflow-hidden flex px-8"
+  >
+    <text-logo>Club Mania</text-logo>
     <ol class="flex">
       <li>
         <router-link :to="'#'">
@@ -23,10 +25,10 @@
         </router-link>
       </li>
     </ol>
-    <div id="#searchBar">
-      <span>Icon1</span> <!---Wait for adding icon plugin--->
-      <input type="text" placeholder="ชื่อชมรม" />
-      <span>Icon2</span> <!---Wait for adding icon plugin--->
+    <div id="#searchBar" class="flex items-center">
+      <mdi-account-circle-outline class="w-24px h-auto" />
+      <input type="text" placeholder="ชื่อชมรม" class="search-input bg-transparent max-w-133px h-32px ml-8px mr-24px" />
+      <mdi-magnify class="w-24px h-auto" />
     </div>
   </div>
 </template>
@@ -37,12 +39,22 @@
 
 <style scoped>
 .headbar {
+  color: white;
   background: rgba(0, 0, 0, 0.5);
   align-items: center;
   justify-content: space-between;
 }
 
+.search-input {
+  @apply font-Roboto font-500 text-[20px] leading-[24px] tracking-[0.1px];
+}
+
 li {
+  @apply font-Roboto font-500 text-[20px] leading-[24px] tracking-[0.1px];
+  display: flex;
+  align-items: center;
+  height: 64px;
+  padding: 0px 16px;
   margin-right: 48px;
 }
 
