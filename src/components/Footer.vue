38824f7237ff4d154f8f91bb3f6346ba2579f5fc
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-23 11:06:53 +0700

    ✨ footer is here

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,13 @@
 <template>
-  
+  <div class="h-[64px] text-white bg-purple-900++ w-full px-[32px] py-[17.45px]">
+    <h5>{{ t("footer.text") }}</h5>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 </script>
 
 <style>
-  
 </style>
