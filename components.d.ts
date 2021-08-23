06f9a46252190a0cd9136f196f5f54d6b6124fd5
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-23 19:19:12 +0700

    👉Add simple Navbar to defualt layout

--- components.d.ts
+++ components.d.ts
@@ -3,15 +3,8 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    CarbonCampsite: typeof import('virtual:vite-icons/carbon/campsite')['default']
-    CarbonDicomOverlay: typeof import('virtual:vite-icons/carbon/dicom-overlay')['default']
-    CarbonLanguage: typeof import('virtual:vite-icons/carbon/language')['default']
-    CarbonLogoGithub: typeof import('virtual:vite-icons/carbon/logo-github')['default']
-    CarbonMoon: typeof import('virtual:vite-icons/carbon/moon')['default']
-    CarbonPedestrian: typeof import('virtual:vite-icons/carbon/pedestrian')['default']
-    CarbonSun: typeof import('virtual:vite-icons/carbon/sun')['default']
-    CarbonWarning: typeof import('virtual:vite-icons/carbon/warning')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
+    Navbar: typeof import('./src/components/Navbar.vue')['default']
     README: typeof import('./src/components/README.md')['default']
   }
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="headbar container h-64px backdrop-blur-14px overflow-hidden flex">
+    <h2>Club Mania</h2>
+    <ol class="flex">
+      <li>
+        <router-link :to="'#'">
+          วิชาการ
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="'#'">
+          ศิลป์วัฒน์
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="'#'">
+          กีฬา
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="'#'">
+          พัฒน์
+        </router-link>
+      </li>
+    </ol>
+    <div id="#searchBar">
+      <span>Icon1</span> <!---Wait for adding icon plugin--->
+      <input type="text" placeholder="ชื่อชมรม" />
+      <span>Icon2</span> <!---Wait for adding icon plugin--->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.headbar {
+  background: rgba(0, 0, 0, 0.5);
+  align-items: center;
+  justify-content: space-between;
+}
+
+li {
+  margin-right: 48px;
+}
+
+li:last-child {
+  margin-right: 0px;
+}
+
+</style>
