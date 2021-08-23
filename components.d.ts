18886d16019b145f9fd066bafb919fd3ca02abf1
miello <partearth1212@gmail.com>
2021-08-24 00:04:31 +0700

    💄 Implement Vichagarn image frame

--- components.d.ts
+++ components.d.ts
@@ -3,19 +3,19 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    CarbonCloseFilled: typeof import('virtual:vite-icons/carbon/close-filled')['default']
-    BackgrondSection: typeof import('./src/components/BackgrondSection.vue')['default']
     BackgroundSection: typeof import('./src/components/BackgroundSection.vue')['default']
-    CarbonWarning: typeof import('virtual:vite-icons/carbon/warning')['default']
+    CarbonCloseFilled: typeof import('virtual:vite-icons/carbon/close-filled')['default']
     Filter: typeof import('./src/components/Filter.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
+    GeraCard: typeof import('./src/components/Card/Gera/gera-card.vue')['default']
     MdiAccountCircleOutline: typeof import('virtual:vite-icons/mdi/account-circle-outline')['default']
     MdiMagnify: typeof import('virtual:vite-icons/mdi/magnify')['default']
     Navbar: typeof import('./src/components/Navbar.vue')['default']
     README: typeof import('./src/components/README.md')['default']
+    SlipvatCard: typeof import('./src/components/Card/SlipVat/slipvat-card.vue')['default']
+    SlipvatImg: typeof import('./src/components/Card/SlipVat/slipvat-img.vue')['default']
     TextBody1: typeof import('./src/components/typography/text-body1.vue')['default']
     TextBody2: typeof import('./src/components/typography/text-body2.vue')['default']
-    TextBox: typeof import('./src/components/BackgroundSection.vue')['default']
     TextCaption: typeof import('./src/components/typography/text-caption.vue')['default']
     TextFrame: typeof import('./src/components/TextFrame.vue')['default']
     TextLogo: typeof import('./src/components/typography/text-logo.vue')['default']
@@ -23,6 +23,9 @@ declare module 'vue' {
     TextSub1: typeof import('./src/components/typography/text-sub1.vue')['default']
     TextSub2: typeof import('./src/components/typography/text-sub2.vue')['default']
     ToggleThing: typeof import('./src/components/ToggleThing.vue')['default']
+    VichagarnCard: typeof import('./src/components/Card/Vichakarn/vichagarn-card.vue')['default']
+    VichagarnTextCard: typeof import('./src/components/Card/Vichakarn/vichagarn-text-card.vue')['default']
+    VichakarnImageCard: typeof import('./src/components/Card/Vichakarn/vichakarn-image-card.vue')['default']
   }
 }
 
---
+++ src/components/Card/VichagarnCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="outer-frame">
+    <div class="inner-frame">
+      <img src="https://dummyimage.com/200x200/5c205c/ffffff.png">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer-frame {
+    border-color:#228AC5 #0B2E58 #0C233F #0C5AA3;
+    border-image:none;
+    border-radius: 0 0 0 0;
+    -moz-border-radius:0 0 0 0;
+    -webkit-border-radius:0 0 0 0;
+    border-style:solid;
+    border-width:8px;
+    height:200px;
+    width:200px;
+}
+.inner-frame {
+    width: 100%;
+    height: 100%;
+    border-color: #0D3D78;
+    border-width: 8px;
+}
+</style>
