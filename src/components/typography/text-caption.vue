80c0374d1326f0f69b2ccb0fb565eccfc080f847
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-23 13:12:42 +0700

    💄 added all typography styles (Retro Gaming???)

---
+++ src/components/typography/text-caption.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="font-Mitr font-300 text-[12px] tracking-[0.4px]">
+    <slot></slot>
+  </span>
+</template>
---
+++ src/components/typography/text-overline.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="font-Roboto font-300 text-[10px] leading-[16px] tracking-[1.5px] uppercase">
+    <slot></slot>
+  </span>
+</template>
