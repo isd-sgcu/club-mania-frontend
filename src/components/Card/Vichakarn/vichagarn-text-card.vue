fedf8f8ae36c65a0a6c1dc1c451f9b1b1893919d
miello <partearth1212@gmail.com>
2021-08-24 21:47:11 +0700

    💄 Make image url and text props component

---
+++ src/components/Card/Vichakarn/vichagarn-text-card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="outer-frame p-2 max-w-[200px] box-content text-center bg-[#0D3D78] text-white">
+    <text-sub2><slot /></text-sub2>
+  </div>
+</template>
+
+<style scoped>
+.outer-frame {
+    @apply rounded-none border-solid border-width-8;
+    border-color: #228ac5 #0b2e58 #0c233f #0c5aa3;
+}
+</style>
---
+++ src/components/Card/Vichakarn/vichakarn-image-card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { defineProps, toRefs } from 'vue'
+
+const props = defineProps({
+  imgUrl: String,
+})
+
+const { imgUrl } = toRefs(props)
+
+</script>
+
+<template>
+  <div class="outer-frame mb-3">
+    <div class="inner-frame">
+      <img :src="imgUrl">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer-frame {
+    @apply rounded-none border-solid border-width-8;
+    border-color:#228AC5 #0B2E58 #0C233F #0C5AA3;
+}
+.inner-frame {
+    @apply w-full h-full border-width-8 max-w-[200px];
+    border-color: #0D3D78;
+}
+</style>
