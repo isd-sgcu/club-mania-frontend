32a855b7ed87af0c5c2dc8a2f3454fff4f41a892
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-24 12:25:36 +0700

    🤡 example of <ToggleThing>

---
+++ src/pages/example/toggle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full px-4 h-screen bg-dark-900 grid items-center">
+    <div class="space-y-3">
+      <ToggleThing @toggle="(activeState) => isToggleActive = activeState" />
+      <text-logo class="text-white block">Toggle is now {{ isToggleActive ? 'active' : 'disabled' }}</text-logo>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isToggleActive = ref<null | boolean>(null);
+</script>
+
+<style>
+</style>
