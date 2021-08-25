32550e50c6f2cfd3a2da6344a954e3407289190c
miello <partearth1212@gmail.com>
2021-08-25 18:06:43 +0700

    💄 Fix filter for webkit

--- src/components/Card/Gera/gera-card.vue
+++ src/components/Card/Gera/gera-card.vue
@@ -26,6 +26,7 @@ const { imgUrl, text, dark } = toRefs(props)
 .light {
     @apply w-max p-2 max-w-[200px] box-content pb-0;
     filter: drop-shadow(0px 0px 5px #FF3333);
+    -webkit-filter: drop-shadow(0px 0px 5px #FF3333);
     background: linear-gradient(180deg, #DF322A 0%, #C0261F 100%);
 }
 .dark {
