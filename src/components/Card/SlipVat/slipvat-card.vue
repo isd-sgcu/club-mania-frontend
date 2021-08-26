000488bc3bb78604c09d0dfdb025c911cd8b042d
miello <partearth1212@gmail.com>
2021-08-26 12:57:45 +0700

    ✨ Implement Slipvat card

---
+++ src/components/Card/SlipVat/slipvat-card.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps({
+  dark: Boolean,
+  text: String,
+})
+
+const { dark, text } = toRefs(props)
+
+</script>
+
+<template>
+  <div class="max-w-[200px]">
+    <img :class="dark ? 'outer-frame-pink' : 'outer-frame-blue'" class="rounded-2xl w-full" src="https://dummyimage.com/200x200/5c205c/ffffff.png">
+    <div :class="dark ? 'outer-frame-pink' : 'outer-frame-blue'" class="bg-[#1F0A41] text-white pt-4 pb-4 pl-6 pr-6 rounded-2xl text-center mt-4">
+      <text-sub2 class="leading-6">
+        {{ text }}
+      </text-sub2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer-frame-pink {
+  border: 2px solid #FB39CF;
+  filter: drop-shadow(0px 0px 4px #FF82E3);
+}
+
+.outer-frame-blue {
+  border: 2px solid #7EECF9;
+  filter: drop-shadow(0px 0px 4px #7EECF9);
+}
+</style>
