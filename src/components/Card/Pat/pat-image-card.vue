73463441ab23d64ad68afcc038d63287be9c88b8
miello <partearth1212@gmail.com>
2021-08-26 16:28:32 +0700

    ✨ Implement skeleton of pat card

---
+++ src/components/Card/Pat/pat-image-card.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <PatBorderCard>
+    <img src="https://dummyimage.com/500x500/5c205c/ffffff.png">
+  </PatBorderCard>
+</template>
--- src/pages/example/Card.vue
+++ src/pages/example/Card.vue
@@ -1,5 +1,6 @@
 <template>
-  <SlipvatCard dark text="ชมรมศิลปะการพูดและการแสดง" />
+  <PatCard />
+  <!-- <SlipvatCard dark text="ชมรมศิลปะการพูดและการแสดง" />
   <GeraCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="ชมรม Hello World" dark />
-  <VichagarnCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="Hello World" />
+  <VichagarnCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="Hello World" /> -->
 </template>
