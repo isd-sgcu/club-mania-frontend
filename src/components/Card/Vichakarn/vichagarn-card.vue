aa54b2a686525a1303a1756b442456b2b85e2bc7
miello <partearth1212@gmail.com>
2021-08-26 18:08:11 +0700

    🐛 Fix width of gera and vichagarn card

--- src/components/Card/Vichakarn/vichagarn-card.vue
+++ src/components/Card/Vichakarn/vichagarn-card.vue
@@ -10,7 +10,7 @@ defineProps<IProps>()
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-max">
+  <div class="flex flex-col items-center w-max max-w-[200px]">
     <vichakarn-image-card :img-url="imgUrl"></vichakarn-image-card>
     <vichagarn-text-card>{{ text }}</vichagarn-text-card>
   </div>
