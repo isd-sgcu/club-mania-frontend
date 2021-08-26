5f0217245c5a21c9b217427921f5e152d6682652
miello <partearth1212@gmail.com>
2021-08-26 17:02:27 +0700

    💄 Bind image url and text to card

--- src/components/Card/Pat/pat-card.vue
+++ src/components/Card/Pat/pat-card.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import { defineProps, toRefs } from 'vue'
+
+const props = defineProps({
+  imgUrl: String,
+  text: String,
+})
+
+const { imgUrl, text } = toRefs(props)
+
+</script>
+
 <template>
   <div>
-    <PatImageCard class="mb-2" />
-    <PatTextCard />
+    <PatImageCard class="mb-2" :img-url="imgUrl" />
+    <PatTextCard>{{ text }}</PatTextCard>
   </div>
 </template>
--- src/components/Card/Pat/pat-image-card.vue
+++ src/components/Card/Pat/pat-image-card.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { defineProps, toRefs } from 'vue'
+
+const props = defineProps({
+  imgUrl: String,
+})
+
+const { imgUrl } = toRefs(props)
+
 </script>
 
 <template>
   <PatBorderCard>
-    <img src="https://dummyimage.com/500x500/5c205c/ffffff.png">
+    <img :src="imgUrl || 'https://dummyimage.com/500x500/5c205c/ffffff.png'">
   </PatBorderCard>
 </template>
