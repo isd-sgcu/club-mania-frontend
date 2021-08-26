3a3017287f19704d657f0b0640d4ad070f65a518
miello <partearth1212@gmail.com>
2021-08-26 18:08:11 +0700

    🐛 Fix width of gera and vichagarn card

--- src/components/Card/Gera/gera-card.vue
+++ src/components/Card/Gera/gera-card.vue
@@ -12,7 +12,7 @@ defineProps<IProps>()
 </script>
 
 <template>
-  <div class="flex flex-col w-max box-border ">
+  <div class="flex flex-col w-max box-border">
     <div :class="dark ? 'dark' : 'light'">
       <img class="w-full" :src="imgUrl">
       <div class="inner-frame">
@@ -24,13 +24,13 @@ defineProps<IProps>()
 
 <style scoped>
 .light {
-    @apply w-max p-2 max-w-[200px] box-content pb-0;
+    @apply w-max p-2 max-w-[200px] pb-0;
     filter: drop-shadow(0px 0px 5px #FF3333);
     -webkit-filter: drop-shadow(0px 0px 5px #FF3333);
     background: linear-gradient(180deg, #DF322A 0%, #C0261F 100%);
 }
 .dark {
-    @apply w-max p-2 max-w-[200px] box-content pb-0;
+    @apply w-max p-2 max-w-[200px] pb-0;
     background: linear-gradient(148.99deg, #770000 0%, #C80000 34.94%, #770000 69.5%);
 }
 
