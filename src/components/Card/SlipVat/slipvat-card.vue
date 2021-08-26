49c522528e055bcc13e1e057d7616ad0e93e6d8d
miello <partearth1212@gmail.com>
2021-08-26 13:47:22 +0700

    💄 Change some text border color

--- src/components/Card/SlipVat/slipvat-card.vue
+++ src/components/Card/SlipVat/slipvat-card.vue
@@ -10,8 +10,8 @@ const { dark, text } = toRefs(props)
 
 <template>
   <div class="max-w-[200px]">
-    <img :class="dark ? 'outer-frame-pink' : 'outer-frame-blue'" class="rounded-2xl w-full" src="https://dummyimage.com/200x200/5c205c/ffffff.png">
-    <div :class="dark ? 'outer-frame-pink' : 'outer-frame-blue'" class="bg-[#1F0A41] text-white pt-4 pb-4 pl-6 pr-6 rounded-2xl text-center mt-4">
+    <img :class="'outer-frame-' + (dark ? 'pink' : 'blue')" class="rounded-2xl w-full" src="https://dummyimage.com/200x200/5c205c/ffffff.png">
+    <div :class="'text-frame-' + (dark ? 'pink' : 'blue')" class="bg-[#1F0A41] text-white pt-4 pb-4 pl-6 pr-6 rounded-2xl text-center mt-4">
       <text-sub2 class="leading-6">
         {{ text }}
       </text-sub2>
@@ -27,6 +27,16 @@ const { dark, text } = toRefs(props)
 
 .outer-frame-blue {
   border: 2px solid #7EECF9;
+  filter: drop-shadow(0px 0px 4px #7EECF9);
+}
+
+.text-frame-pink {
+  border: 2px solid #FFCDF4;
+  filter: drop-shadow(0px 0px 4px #FF82E3);
+}
+
+.text-frame-blue {
+  border: 2px solid #C2F9FF;
   filter: drop-shadow(0px 0px 4px #7EECF9);
 }
 </style>
