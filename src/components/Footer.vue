af099682af0690895626de1f69fb94d0216aa07a
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-27 13:50:45 +0700

    🎨Footer and PageBackground can change theme now

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <div
-    class="h-[64px] text-white backdrop-blur-14px
-          w-full px-[32px] py-[17.45px] bottom-0"
-    style="background: rgba(0, 0, 0, 0.6);"
+    class="h-[64px] text-white backdrop-blur-14px w-full px-[32px] py-[17.45px] bottom-0"
+    :class="`bg-${color}`"
   >
     <h5>{{ t("footer.text") }}</h5>
   </div>
@@ -11,6 +10,20 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
+
+const props = defineProps({
+  theme: String,
+})
+
+const bgColors = {
+  Vichakarn: '#0C233F',
+  Slipvat: '#1B0D31',
+  Gera: '#560000',
+  Pat: '#914D1A',
+  Other: '#000D39',
+}
+
+const color = props.theme ? bgColors.Vichakarn : '#0C0C0C'
 </script>
 
 <style>
---
+++ src/components/PageBackground.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="background" :class="genre"></div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  theme: String,
+})
+</script>
+<style>
+.background {
+  @apply fixed w-full h-full -z-50;
+  background: linear-gradient(180deg, #1F1F1F 29.1%, #212121 100%);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.background.vichakarn {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), linear-gradient(180deg, #04489A 28.91%, #0B294E 92.96%);
+  box-shadow: none;
+}
+.background.slipvat {
+  background: linear-gradient(180deg, #1F0A41 29.1%, #4F1B6F 100%);
+  box-shadow: none;
+}
+
+.background.gera {
+  background: linear-gradient(180deg, #390000 60.73%, #800000 100%);
+  box-shadow: none;
+}
+
+.background.pat {
+  background: linear-gradient(180deg, #FFF2D7 58.85%, #FDD37E 99.99%, #FFCB64 100%);
+  box-shadow: none;
+}
+
+.background.other {
+  background: linear-gradient(180deg, #35638E 26.29%, #00203E 100%);
+  box-shadow: none;
+}
+</style>
