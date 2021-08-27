57afeec2884e0f079a06aeb23f88685ebc250a94
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-27 08:54:31 +0700

    🏷️ ThemeOption, maybe useful as a prop type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 import { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+// IMPORTANT some colors in Figma are named 'Vishagarn' not 'Vichagarn', so watchout if that's the case
+export type ThemeOption = 'SilpVat' | 'Vichagarn' | 'Gera' | 'Pat' | 'Other'
