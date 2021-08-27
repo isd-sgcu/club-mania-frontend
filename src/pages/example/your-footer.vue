bfef2ccfb025a66ec651f9a4ed4e8293be46c470
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 02:23:37 +0700

    🤡 example of footer

---
+++ src/pages/example/your-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mb-20"></div>
+  <Footer theme="Other" />
+  <Footer theme="Pat" />
+  <Footer theme="Gera" />
+  <Footer theme="Vichagarn" />
+  <Footer theme="SilpVat" />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<route lang="yaml">
+meta:
+  layout: 404
+</route>
