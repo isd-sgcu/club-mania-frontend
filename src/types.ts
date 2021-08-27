535a3ad0cb58f7e2fa63501972970d0adf5983f7
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 02:52:20 +0700

    🏷️ 'Main' is now a theme option, landing page theme?

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,4 @@ import { ViteSSGContext } from 'vite-ssg'
 export type UserModule = (ctx: ViteSSGContext) => void
 
 // IMPORTANT some colors in Figma are named 'Vishagarn' not 'Vichagarn', so watchout if that's the case
-export type ThemeOption = 'SilpVat' | 'Vichagarn' | 'Gera' | 'Pat' | 'Other'
+export type ThemeOption = 'SilpVat' | 'Vichagarn' | 'Gera' | 'Pat' | 'Other' | 'Main'
