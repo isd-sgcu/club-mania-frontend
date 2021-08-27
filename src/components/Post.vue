7432d88f7d3c37adec0c062a0cf11f12152a0a75
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 01:39:35 +0700

    🍻 messy club page draft bruh

---
+++ src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <BackgroundSection :theme="themeStore.savedTheme">
+  <h3>おはよう</h3>
+  </BackgroundSection>
+</template>
+
+<script setup lang="ts">
+import { useThemeStore } from '~/stores/themes'
+const themeStore = useThemeStore()
+
+</script>
+
+<style>
+</style>
---
+++ src/layouts/club.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+  <main>
+    <router-view />
+  </main>
+  <Footer :theme="name" />
+</template>
+
+<script setup lang="ts">
+import { useThemeStore } from '~/stores/themes'
+
+const theme = useThemeStore()
+const name = ref(theme.savedTheme)
+
+</script>
