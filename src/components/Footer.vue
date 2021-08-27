777c9caa184c06686581f121de6bd7e553138b86
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-27 17:40:09 +0700

    Footer and PageBackground can change theme

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="h-[64px] text-white backdrop-blur-14px w-full px-[32px] py-[17.45px] bottom-0"
-    :class="`bg-${color}`"
+    :class="`bg-[${color}]`"
   >
     <h5>{{ t("footer.text") }}</h5>
   </div>
@@ -9,15 +9,16 @@
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { ThemeOption } from '~/types'
 const { t } = useI18n()
 
-const props = defineProps({
-  theme: String,
-})
+const props = defineProps<{
+  theme: ThemeOption
+}>()
 
 const bgColors = {
   Vichakarn: '#0C233F',
-  Slipvat: '#1B0D31',
+  Silpvat: '#1B0D31',
   Gera: '#560000',
   Pat: '#914D1A',
   Other: '#000D39',
--- src/components/PageBackground.vue
+++ src/components/PageBackground.vue
@@ -1,40 +1,32 @@
 <template>
-  <div class="background" :class="theme"></div>
+  <div class="fixed w-full h-full -z-50" :style="styleObject"></div>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  theme: String,
-})
+import { ThemeOption } from '~/types'
+
+const props = defineProps<{
+  theme: ThemeOption
+}>()
+
+const pageBgColors = {
+  Vichagarn: {
+    background: 'linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), linear-gradient(180deg, #04489A 28.91%, #0B294E 92.96%)',
+  },
+  SilpVat: {
+    background: 'linear-gradient(180deg, #1F0A41 29.1%, #4F1B6F 100%)',
+  },
+  Gera: {
+    background: 'linear-gradient(180deg, #390000 60.73%, #800000 100%)',
+  },
+  Pat: {
+    background: 'linear-gradient(180deg, #FFF2D7 58.85%, #FDD37E 99.99%, #FFCB64 100%)',
+  },
+  Other: {
+    background: 'linear-gradient(180deg, #35638E 26.29%, #00203E 100%)',
+  },
+}
+
+const styleObject = props.theme ? pageBgColors[props.theme] : { background: 'linear-gradient(180deg, #1F1F1F 29.1%, #212121 100%)' }
+
 </script>
-<style>
-.background {
-  @apply fixed w-full h-full -z-50;
-  background: linear-gradient(180deg, #1F1F1F 29.1%, #212121 100%);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-
-.background.vichakarn {
-  background: linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), linear-gradient(180deg, #04489A 28.91%, #0B294E 92.96%);
-  box-shadow: none;
-}
-.background.slipvat {
-  background: linear-gradient(180deg, #1F0A41 29.1%, #4F1B6F 100%);
-  box-shadow: none;
-}
-
-.background.gera {
-  background: linear-gradient(180deg, #390000 60.73%, #800000 100%);
-  box-shadow: none;
-}
-
-.background.pat {
-  background: linear-gradient(180deg, #FFF2D7 58.85%, #FDD37E 99.99%, #FFCB64 100%);
-  box-shadow: none;
-}
-
-.background.other {
-  background: linear-gradient(180deg, #35638E 26.29%, #00203E 100%);
-  box-shadow: none;
-}
-</style>
