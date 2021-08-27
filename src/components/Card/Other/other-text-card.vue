6fa71ddac5e9382b7758e0dbd57cc88e09886868
miello <partearth1212@gmail.com>
2021-08-27 10:59:37 +0700

    🎨 Seperate background and ghost to folder

--- src/components/Card/Other/other-text-card.vue
+++ src/components/Card/Other/other-text-card.vue
@@ -2,10 +2,10 @@
   <div class="w-[200px]">
     <div class="relative">
       <img class="absolute top-0 w-full h-full img-clip" src="https://dummyimage.com/200x200/5c205c/ffffff.png">
-      <Ghost class="w-full h-full mb-4" preserveAspectRatio="none" />
+      <PinkGhost class="w-full h-full mb-4" preserveAspectRatio="none" />
     </div>
     <div class="relative">
-      <PixelBackground class="w-full h-full" preserveAspectRatio="none" />
+      <PinkBackground class="w-full h-full" preserveAspectRatio="none" />
       <text-body2 class="absolute break-words top-0 p-5 h-[88px] pt-3 text-white text-center">
         ชมรมอื่นๆ ที่ชื่อยาวนิดหน่อย dddd dddddsdsd dsdsd
       </text-body2>
