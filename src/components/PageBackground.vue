c7eae25a7abebc679ffa23b7471e119b87bf3557
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 02:33:18 +0700

    🚧 <PageBackground> route theme

--- src/components/PageBackground.vue
+++ src/components/PageBackground.vue
@@ -1,32 +1,29 @@
 <template>
-  <div class="fixed w-full h-full -z-50" :style="styleObject"></div>
+  <div
+    class="fixed w-full h-full -z-50"
+    :style="{
+      background
+    }"
+  ></div>
 </template>
 
 <script setup lang="ts">
+import { useThemeStore } from '~/stores/themes'
 import { ThemeOption } from '~/types'
+const themeStore = useThemeStore()
 
 const props = defineProps<{
-  theme: ThemeOption
+  theme?: ThemeOption
 }>()
 
 const pageBgColors = {
-  Vichagarn: {
-    background: 'linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), linear-gradient(180deg, #04489A 28.91%, #0B294E 92.96%)',
-  },
-  SilpVat: {
-    background: 'linear-gradient(180deg, #1F0A41 29.1%, #4F1B6F 100%)',
-  },
-  Gera: {
-    background: 'linear-gradient(180deg, #390000 60.73%, #800000 100%)',
-  },
-  Pat: {
-    background: 'linear-gradient(180deg, #FFF2D7 58.85%, #FDD37E 99.99%, #FFCB64 100%)',
-  },
-  Other: {
-    background: 'linear-gradient(180deg, #35638E 26.29%, #00203E 100%)',
-  },
+  Vichagarn: 'linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), linear-gradient(180deg, #04489A 28.91%, #0B294E 92.96%)',
+  SilpVat: 'linear-gradient(180deg, #1F0A41 29.1%, #4F1B6F 100%)',
+  Gera: 'linear-gradient(180deg, #390000 60.73%, #800000 100%)',
+  Pat: 'linear-gradient(180deg, #FFF2D7 58.85%, #FDD37E 99.99%, #FFCB64 100%)',
+  Other: 'linear-gradient(180deg, #35638E 26.29%, #00203E 100%)',
 }
 
-const styleObject = props.theme ? pageBgColors[props.theme] : { background: 'linear-gradient(180deg, #1F1F1F 29.1%, #212121 100%)' }
+const background = props.theme ? pageBgColors[props.theme] : themeStore.savedTheme
 
 </script>
