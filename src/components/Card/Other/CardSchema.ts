a042713458e14b100726ea5d89aafbb213ea57bd
miello <partearth1212@gmail.com>
2021-08-27 12:32:51 +0700

    ⚡️ Bind remaining other card color

---
+++ src/components/Card/Other/CardSchema.ts
@@ -0,0 +1,31 @@
+import DarkBlueBackground from './Background/darkblue.svg'
+import LightCyanBackground from './Background/lightcyan.svg'
+import PinkBackground from './Background/pink.svg'
+import RedBackground from './Background/red.svg'
+import SteelBackground from './Background/steel.svg'
+import YellowBackground from './Background/yellow.svg'
+
+import DarkBlueGhost from './Ghost/darkblue.svg'
+import LightCyanGhost from './Ghost/lightcyan.svg'
+import PinkGhost from './Ghost/pink.svg'
+import RedGhost from './Ghost/red.svg'
+import SteelGhost from './Ghost/steel.svg'
+import YellowGhost from './Ghost/yellow.svg'
+
+export const BACKGROUND = {
+  darkblue: DarkBlueBackground,
+  lightcyan: LightCyanBackground,
+  pink: PinkBackground,
+  red: RedBackground,
+  steel: SteelBackground,
+  yellow: YellowBackground,
+}
+
+export const GHOST = {
+  darkblue: DarkBlueGhost,
+  lightcyan: LightCyanGhost,
+  pink: PinkGhost,
+  red: RedGhost,
+  steel: SteelGhost,
+  yellow: YellowGhost,
+}
--- src/components/Card/Other/other-card.vue
+++ src/components/Card/Other/other-card.vue
@@ -2,10 +2,10 @@
   <div class="w-[200px]">
     <div class="relative">
       <img class="absolute top-0 w-full h-full img-clip" src="https://dummyimage.com/200x200/5c205c/ffffff.png">
-      <img :src="img" class="w-full h-full mb-4" preserveAspectRatio="none">
+      <img :src="ghostImg" class="w-full h-full mb-4" preserveAspectRatio="none">
     </div>
     <div class="relative">
-      <img src="./Background/darkblue.svg" class="w-full h-full" preserveAspectRatio="none">
+      <img :src="backgroundImg" class="w-full h-full" preserveAspectRatio="none">
       <text-body2 class="absolute break-words top-0 p-5 h-[88px] pt-3 text-center" :class="`text-${text}`">
         ชมรมอื่นๆ ที่ชื่อยาวนิดหน่อย dddd dddddsdsd dsdsd
       </text-body2>
@@ -14,19 +14,21 @@
 </template>
 
 <script setup lang="ts">
-import img from './Ghost/pink.svg'
 import TextColor from './text-color.json'
-
-type GhostColorType = 'darkblue' | 'lightcyan' | 'pink' | 'red' | 'steel' | 'yellow'
+import { BACKGROUND, GHOST } from './CardSchema'
+import { GhostColorType } from './types'
 
 interface IProps {
-  ghostColor: GhostColorType
+  color: GhostColorType
   text?: string
   imgUrl?: string
 }
 
-const { ghostColor } = defineProps<IProps>()
-const text = TextColor[ghostColor]
+const { color } = defineProps<IProps>()
+
+const text = TextColor[color]
+const backgroundImg = BACKGROUND[color]
+const ghostImg = GHOST[color]
 
 </script>
 
