64e03f4e38fc6938d4959b20aad43e01fd0a2fa9
miello <partearth1212@gmail.com>
2021-08-27 12:32:51 +0700

    ⚡️ Bind remaining other card color

---
+++ src/components/Card/Other/types.ts
@@ -0,0 +1,1 @@
+export type GhostColorType = 'darkblue' | 'lightcyan' | 'pink' | 'red' | 'steel' | 'yellow'
--- src/pages/example/Card.vue
+++ src/pages/example/Card.vue
@@ -1,7 +1,22 @@
 <template>
-  <PatCard img-url="https://dummyimage.com/500x500/5c205c/ffffff.png" color="green" text="เยาวชนจิตอาสา (Youth)" />
+  <div class="flex flex-wrap">
+    <PatCard v-for="item in patColor" :key="item" :color="item" img-url="https://dummyimage.com/500x500/5c205c/ffffff.png" text="เยาวชนจิตอาสา (Youth)" />
+  </div>
   <SlipvatCard dark text="ชมรมศิลปะการพูดและการแสดง" />
-  <OtherCard ghost-color="lightcyan" />
-  <GeraCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="ชมรม Hello World" dark />
+  <div class="flex flex-wrap">
+    <OtherCard v-for="item in cardColor" :key="item" :color="item" />
+  </div>
+  <div class="flex flex-wrap">
+    <GeraCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="ชมรม Hello World" dark />
+  </div>
   <VichagarnCard img-url="https://dummyimage.com/200x200/5c205c/ffffff.png" text="Hello World" />
 </template>
+
+<script setup lang="ts">
+import { GhostColorType } from '~/components/Card/Other/types'
+import { PaletteKey } from '~/components/Card/Pat/types'
+
+const cardColor = ['darkblue', 'lightcyan', 'pink', 'red', 'steel', 'yellow'] as GhostColorType[]
+const patColor = ['red', 'blue', 'yellow', 'green'] as PaletteKey[]
+
+</script>
