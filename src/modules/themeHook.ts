60c5503fbb55cbafbe6ba2daecd838f98bc138c1
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 01:56:28 +0700

    🐛 fixed undefined theme if no mapping of route

--- src/modules/themeHook.ts
+++ src/modules/themeHook.ts
@@ -13,7 +13,10 @@ export const install: UserModule = ({ router, isClient }) => {
     const store = useThemeStore()
     router.afterEach(() => {
       const path = router.currentRoute.value.path
-      store.setNewTheme(themeMap[path])
+      const themeOfThisPath = themeMap[path]
+      // themeOfThisPath could be undefined if the route hasn't been registered in the themeMap above.
+      if (themeOfThisPath)
+        store.setNewTheme(themeMap[path])
     })
   }
 }
