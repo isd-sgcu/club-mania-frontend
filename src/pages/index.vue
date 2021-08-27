5467d5dfd2588118999cec6491f62730e73f8801
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-27 13:52:52 +0700

    👉Landing Page with dummy Carousel

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>Hi</h1>
+  <PageBackground :genre="name" />
+  <div class="wrapper">
+    <Carousel />
+    <h1>สำรวจชมรม</h1>
+    <button @click="goNextTheme">
+      click me
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useThemeStore } from '~/stores/themes'
 
+const theme = useThemeStore()
+const name = ref(theme.savedTheme)
+
+let idx = 0
+
+const goNextTheme = () => {
+  idx = (idx + 1) % 6
+  theme.setNewTheme(idx)
+}
 </script>
 
 <style>
+.wrapper {
+  @apply w-full min-h-960px text-center;
+}
 </style>
