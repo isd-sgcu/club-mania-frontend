1c25c4decce1b338dec810f0e21651df306dde6e
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-28 21:00:44 +0700

    import Swiper (slider component library)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,25 @@
 <template>
   <!---Slide show_-->
-  <Carousel />
+  <swiper :pagination="true" class="h-78 w-full">
+    <swiper-slide class="bg-black">
+      Club Mania 2021
+    </swiper-slide>
+    <swiper-slide class="bg-rose-500">
+      ฝ่ายกีฬา
+    </swiper-slide>
+    <swiper-slide class="bg-yellow-500">
+      ฝ่ายพัฒน์
+    </swiper-slide>
+    <swiper-slide class="bg-blue-500">
+      ฝ่ายวิชาการ
+    </swiper-slide>
+    <swiper-slide class="bg-purple-500">
+      ฝ่ายศิลป์วัฒน์
+    </swiper-slide>
+    <swiper-slide class="bg-navy">
+      ฝ่ายอื่นๆ
+    </swiper-slide>
+  </swiper>
   <!---Main content-->
   <div class=" w-full flex flex-col items-center py-12 ">
     <h3 class="mb-48px">
@@ -22,6 +41,20 @@
 </template>
 
 <script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination,
+} from 'swiper'
+
+// install Swiper modules
+SwiperCore.use([Pagination])
 
 </script>
 
