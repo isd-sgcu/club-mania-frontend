10e4415db60bfe710a27f8ad58be76cad5d43935
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 14:54:44 +0700

    🏷️ dummy post type

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,16 @@ export type UserModule = (ctx: ViteSSGContext) => void
 
 // IMPORTANT some colors in Figma are named 'Vishagarn' not 'Vichagarn', so watchout if that's the case
 export type ThemeOption = 'SilpVat' | 'Vichagarn' | 'Gera' | 'Pat' | 'Other' | 'Main'
+
+export type Post = {
+  publisher: string // later should be user type
+  text: string
+  badge: string
+  likes: number
+  postedAt: string
+  replies: {
+    publisher: string // later should be user type
+    repliedAt: number
+    likes: number
+  }[]
+}
