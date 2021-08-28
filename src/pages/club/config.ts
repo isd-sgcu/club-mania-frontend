0e8bec65c2922c1eef1b63417dd8367b772ab569
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 14:55:09 +0700

    💄 ui config in club page

---
+++ src/pages/club/config.ts
@@ -0,0 +1,22 @@
+const useClubConfig = () => {
+  return {
+    clubTypeColor: {
+      SilpVat: '[#BB62DB]',
+      Vichagarn: '[#3DACEB]',
+      Gera: '[#DF322A]',
+      Pat: '[#98521E]',
+      Other: '[#7CB8E7]',
+      Main: 'white',
+    },
+    clubNameColor: {
+      SilpVat: 'white',
+      Vichagarn: 'white',
+      Gera: 'white',
+      Pat: '[#5B3112]',
+      Other: 'white',
+      Main: 'grey-light',
+    },
+  }
+}
+
+export default useClubConfig
