8a5ffdd89e4f5884b709f15aebb43e43b620d8c4
miello <partearth1212@gmail.com>
2021-08-28 17:24:21 +0700

    💄 Update hover vichagarn-card palette

--- src/components/Card/Vichakarn/vichagarn-card.vue
+++ src/components/Card/Vichakarn/vichagarn-card.vue
@@ -7,11 +7,23 @@ interface IProps {
 
 defineProps<IProps>()
 
+const detect = ref(false)
+
+const handleMouseEnter = () => {
+  detect.value = true
+}
+
+const handleMouseOut = () => {
+  detect.value = false
+}
+
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-max max-w-[200px]">
-    <vichakarn-image-card :img-url="imgUrl"></vichakarn-image-card>
-    <vichagarn-text-card>{{ text }}</vichagarn-text-card>
+  <div class="flex flex-col items-center w-max max-w-[200px]" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
+    <vichakarn-image-card :img-url="imgUrl" :hover="detect"></vichakarn-image-card>
+    <vichagarn-text-card :hover="detect">
+      {{ text }}
+    </vichagarn-text-card>
   </div>
 </template>
