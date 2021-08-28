52e3b7dbed0d49e49fe095e3d28b0587b96af09b
Arm <69001162+arskiir@users.noreply.github.com>
2021-08-28 18:25:11 +0700

    🚧 header of post/reply

---
+++ src/components/Post/PostHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center space-x-3">
+    <h2 class="text-[24px] font-Roboto inline">
+      {{ publisher }}
+    </h2>
+    <Badge v-if="badge" :badge-text="badge" />
+    <span>{{ postedAt }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  publisher: string
+  badge?: string
+  postedAt: string
+}>()
+</script>
+
+<style>
+</style>
