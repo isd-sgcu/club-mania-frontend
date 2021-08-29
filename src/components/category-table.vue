2055314241d1b062e681d51bfa556ecd0aa1d086
miello <partearth1212@gmail.com>
2021-08-29 13:25:38 +0700

    💄 Add margin to category table

--- src/components/category-table.vue
+++ src/components/category-table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center mt-4 mb-4">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-4 gap-10">
       <slot />
     </div>
