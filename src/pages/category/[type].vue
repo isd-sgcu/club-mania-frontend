96bcd28213cb50c779a77a51dbd05015a42df0bc
miello <partearth1212@gmail.com>
2021-08-30 00:15:04 +0700

    ✨ Make structure for category page

--- src/pages/category/[type].vue
+++ src/pages/category/[type].vue
@@ -1,52 +1,31 @@
 <template>
-  <!---Slide show--->
-  <swiper
-    :pagination="{clickable: true}"
-    :slides-per-view="1"
-    :loop="true"
-    :autoplay="{'delay': 2500, 'disableOnInteraction': false}"
-    class="w-full"
-  >
-    <!---Each page of the slide-->
-    <swiper-slide v-for="(item, index) in themes" :key="index" class="relative flex items-center justify-center">
-      <Banner :theme="item" :is-club="false" />
-    </swiper-slide>
-  </swiper>
-  <!---Main content-->
-  <div class=" w-full flex flex-col items-center py-12 ">
-    <h3>
-      สำรวจชมรม
-    </h3>
-    <CategoryTable>
-      <MainCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :type="item.variant" />
-    </CategoryTable>
+  <div class="h-screen">
+    Hello World
+    <div class=" w-full flex flex-col items-center py-12 ">
+      <h3>
+        สำรวจชมรม
+      </h3>
+      <CategoryTable>
+        <PatCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :color="'red'" />
+        <SilpvatCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :type="item.variant" />
+        <OtherCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :color="'red'" />
+        <GeraCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :type="item.variant" />
+        <VichagarnCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :type="item.variant" />
+      </CategoryTable>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue'
+import { useThemeStore } from '~/stores/themes'
 
-// Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/pagination'
+const theme = useThemeStore()
 
-// import Swiper core and required modules
-import SwiperCore, {
-  Autoplay, Pagination,
-} from 'swiper'
-
-// Types
-import { ThemeOption } from '~/types'
-
-// install Swiper modules
-SwiperCore.use([Autoplay, Pagination])
-
-const themes = ['Main', 'Gera', 'Pat', 'Vichagarn', 'SilpVat', 'Other'] as ThemeOption[]
+theme.setNewTheme('Vichagarn')
 
 const data: any = [
   {
-    text: 'ชมรมวิจัย',
+    text: 'ชมรมวิจัยยยย',
     imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
     variant: 'SilpVat',
   }, {
@@ -109,20 +88,5 @@ const data: any = [
     variant: 'Pat',
   },
 ]
+
 </script>
-
-<style>
-.swiper-pagination {
-  transform: translateY(-65%);
-}
-
-.swiper-pagination-bullet {
-  width: 12px;
-  height: 12px;
-  opacity: 1;
-  background: rgba(187, 187, 187, 0.4);
-}
-.swiper-pagination-bullet-active {
-  background: white;
-}
-</style>
