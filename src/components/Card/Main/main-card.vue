ce02c2897b3991cb6f7e0ad52dfee2023ab9db06
miello <partearth1212@gmail.com>
2021-08-29 12:17:42 +0700

    🐛 Fix animation on main-card

--- src/components/Card/Main/main-card.vue
+++ src/components/Card/Main/main-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[200px]"
+    class="w-[200px] transition-all"
     :style="{filter: `drop-shadow(0px 0px ${borderWidth} ${CardBorder})`, border: `${borderWidth} solid ${CardBorder}`, borderRadius: '8px' }"
     @mouseenter="handleMouseEnter"
     @mouseleave="handleMouseOut"
@@ -8,7 +8,7 @@
     <div class="w-full p-3 rounded-tl-md rounded-tr-md" style="background: linear-gradient(160.98deg, #130000 0%, #7B7B7B 34.44%, #130000 68.49%);">
       <img :src="imgUrl">
     </div>
-    <div class="w-full text-white p-4 pt-2 text-center rounded-bl-md rounded-br-md" :style="{ background: CardBackground + 'linear-gradient(138.79deg, #130000 70%, #4F4F4F 85%, #130000 100%)'}">
+    <div class="w-full text-white p-4 pt-2 text-center rounded-bl-md rounded-br-md" :style="{ background: CardBackground + 'linear-gradient(138.79deg, #130000 70%, #4F4F4F 85%, #130000 100%)' }">
       <text-sub2>{{ text }}</text-sub2>
     </div>
   </div>
