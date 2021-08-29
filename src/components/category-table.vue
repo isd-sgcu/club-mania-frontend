74ecd2f80d7b17db70754630caa47d62a5623f0e
miello <partearth1212@gmail.com>
2021-08-29 12:59:51 +0700

    ✨ Add template for category table

---
+++ src/components/category-table.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-4 gap-10">
+      <slot />
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,9 @@
   <div class="w-full min-h-960px text-center">
     <Carousel />
     <h1>สำรวจชมรม</h1>
+    <CategoryTable>
+      <MainCard v-for="(item, index) in data" :key="index" :text="item.text" :img-url="item.imgUrl" :type="item.variant" />
+    </CategoryTable>
   </div>
 </template>
 
@@ -11,6 +14,72 @@ import { useThemeStore } from '~/stores/themes'
 
 const theme = useThemeStore()
 const name = ref(theme.savedTheme)
+
+const data: any = [
+  {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'SilpVat',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Vichagarn',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Other',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Gera',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Pat',
+  },
+  {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'SilpVat',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Vichagarn',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Other',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Gera',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Pat',
+  },
+  {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'SilpVat',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Vichagarn',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Other',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Gera',
+  }, {
+    text: 'ชมรมวิจัย',
+    imgUrl: 'https://dummyimage.com/200x200/5c205c/ffffff.png',
+    variant: 'Pat',
+  },
+]
 </script>
 
 <style>
