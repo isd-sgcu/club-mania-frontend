e11b6d3e8d8fdd2c6e33fc6fa01cedfd67958145
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-29 16:44:27 +0700

    Fix grid layout

--- src/components/category-table.vue
+++ src/components/category-table.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-center mt-4 mb-4">
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-4 gap-10">
+  <div class="flex justify-center mt-12 mb-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10">
       <slot />
     </div>
   </div>
