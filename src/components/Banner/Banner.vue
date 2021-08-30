c772acf6034ca52761b0cd5112d92b58aba3247f
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-31 00:32:09 +0700

    🎨 Banner text styles for each theme

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -3,14 +3,30 @@
 <!-- Last Updated by Kaoklong -->
 
 <template>
-  <img :src="BannerBackground[theme]" :alt="theme" class="bg-cover h-8/25 max-h-312px 2xl:(min-h-330px max-h-600px)">
-  <p class="absolute" :class="`font-${theme}`">
-    {{ displayText }}
-  </p>
+  <div class="relative flex items-center justify-center">
+    <img :src="BannerBackground[theme]" :alt="theme" class="bg-cover h-8/25 min-h-160px md:min-h-280px max-h-312px 2xl:(min-h-330px max-h-600px)">
+    <span v-if="theme === 'Gera'" class="gera-glow">
+      <p class="gera-border">
+        {{ displayText }}
+      </p>
+    </span>
+    <div v-else-if="theme === 'SilpVat'" class="silpvat-glow">
+      <p class="outer">
+        {{ displayText }}
+      </p>
+      <p class="inner">
+        {{ displayText }}
+      </p>
+    </div>
+    <p class="absolute" :class="`font-${theme}`" :style="styleObject">
+      {{ displayText }}
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
 // Types
+import TextStyle from './bannerText.json'
 import { ThemeOption } from '~/types'
 import { BannerBackground } from '~/imagePath'
 
@@ -22,7 +38,7 @@ const props = defineProps<{
 
 // This text is displayed when an user is in a landing, catagory page
 // When an user is a club page, just show the text prop
-const ClubCatagory = {
+const CatagoryText = {
   Main: 'Club Mania 2021',
   Vichagarn: 'ฝ่ายวิชาการ',
   Gera: 'ฝ่ายกีฬา',
@@ -31,9 +47,39 @@ const ClubCatagory = {
   Other: 'ฝ่ายอื่นๆ',
 }
 // Text to display in components
-const displayText = props.isClub ? props.text : ClubCatagory[props.theme]
+const displayText = props.isClub ? props.text : CatagoryText[props.theme]
+const styleObject: any = TextStyle[props.theme]
 </script>
 
 <style>
+.gera-glow {
+  @apply absolute font-Gera md:font-700 text-stroke-3 md:text-stroke-5 xl:text-stroke-8 text-stroke-black;
+  filter: drop-shadow(0px 0px 15px #DF322A);
+}
 
+.gera-border {
+  filter: drop-shadow(0px 0px 1.5px #DE2727);
+}
+
+.silpvat-glow {
+  box-sizing: content-box;
+  @apply absolute font-SilpVat text-white;
+  display: grid;
+}
+
+.silpvat-glow > * {
+  grid-column: 1;
+  grid-row: 1;
+}
+.silpvat-glow .outer {
+  mix-blend-mode: hard-light;
+  border: 5px solid #F5F5F5;
+  filter: blur(76.9px);
+}
+
+.silpvat-glow .inner {
+  mix-blend-mode: soft-light;
+  border: 14.5px solid #BBBBBB;
+  filter: blur(20.6px);
+}
 </style>
