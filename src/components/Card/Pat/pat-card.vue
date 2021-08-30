7deac51da21f2f80d8782d35b52e7cd4dd49d845
miello <partearth1212@gmail.com>
2021-08-30 18:06:01 +0700

    ✨ Make each card can navigate when click

--- src/components/Card/Pat/pat-card.vue
+++ src/components/Card/Pat/pat-card.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
+import { useRouter } from 'vue-router'
 import { PaletteKey } from './types'
 
 interface IProps {
   imgUrl: string
   text: string
   color: PaletteKey
+  href?: string
 }
 
-defineProps<IProps>()
+const router = useRouter()
+const { href } = defineProps<IProps>()
 
 const detect = ref(false)
 
@@ -20,10 +23,14 @@ const handleMouseOut = () => {
   detect.value = false
 }
 
+const handleClick = () => {
+  router.push(href || '#')
+}
+
 </script>
 
 <template>
-  <div @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
+  <div class="cursor-pointer" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
     <PatImageCard :color="!detect ? color : 'hover'" class="mb-2" :img-url="imgUrl" />
     <PatTextCard :color="!detect ? color: 'hover'">
       {{ text }}
--- src/components/Card/Vichakarn/vichagarn-card.vue
+++ src/components/Card/Vichakarn/vichagarn-card.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 
 interface IProps {
   imgUrl: string
   text: string
+  href?: string
 }
 
-defineProps<IProps>()
+const router = useRouter()
+const { href } = defineProps<IProps>()
 
 const detect = ref(false)
 
@@ -17,10 +20,14 @@ const handleMouseOut = () => {
   detect.value = false
 }
 
+const handleClick = () => {
+  router.push(href || '#')
+}
+
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-max max-w-[200px]" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
+  <div class="flex flex-col items-center w-max max-w-[200px] cursor-pointer" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
     <vichakarn-image-card :img-url="imgUrl" :hover="detect"></vichakarn-image-card>
     <vichagarn-text-card :hover="detect">
       {{ text }}
