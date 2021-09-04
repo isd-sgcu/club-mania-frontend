87ed4dd8d7c96db28a79f7052d6f13e9ac0f12bf
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 00:52:53 +0700

    🏷️ define type for club static info

---
+++ src/assets/clubs/geela/tennis.ts
@@ -0,0 +1,10 @@
+import { ClubStaticInfo } from '~/types'
+
+export const info: ClubStaticInfo = {
+  name: 'ชมรมเทนนิส',
+  category: 'ฝ่ายกีฬา',
+  about: 'เกี่ยวกับชมรมเทสนิส',
+  whatToExpect: 'สิ่งที่น้อง ๆ จะได้รับ จากชมรมเทนนิส',
+  recruitmentPeriod: '20 กรกฎาคม - 25 สิงหาคม 2021',
+  contact: 'Instagram: @cuchorus_official\nFacebook: CU Chorus\nYoutube: CU Chorus \nTwitter: CU Chorus',
+}
