c127bbe29fa10766af863928405de5ede3cef7df
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 03:37:31 +0700

    🚧 extract representatives' info

---
+++ members/club_members.py
@@ -0,0 +1,26 @@
+import csv
+import json
+
+# The output json of this will populate the clubMembers collection which store personal info of each club's representatives.
+
+clubToMembers = {}
+with open('yo.csv', encoding='utf8') as f:
+    reader = csv.DictReader(f)
+    for line in reader:
+        processed_club_name = '-'.join(
+            line['ชื่อชมรมแบบเป็นภาษาอังกฤษ (สามารถใช้ภาษาคาราโอเกะได้ถ้าไม่มีจริงๆ)'].lower().replace(',', '').split())
+        nickname = line['ชื่อเล่น']
+        year = int(line['ชั้นปี'].replace(' ', '').replace('ปี', ''))
+        id = line['รหัสนิสิตของตัวแทนชมรม']
+
+        if processed_club_name not in clubToMembers:
+            clubToMembers[processed_club_name] = []
+
+        clubToMembers[processed_club_name].append({
+            'name': nickname,
+            'studentId': id,
+            year: year
+        })
+
+with open('out.json', 'w', encoding="utf8") as f:
+    json.dump(clubToMembers, f, indent=4)
