9e9c76a304cb0ee2d764675e8dff03bd529b64b8
miello <partearth1212@gmail.com>
2021-09-04 21:41:10 +0700

    💄 Make geela card more responsive width

--- src/components/Card/Gera/gera-card.vue
+++ src/components/Card/Gera/gera-card.vue
@@ -28,9 +28,9 @@ const handleClick = () => {
 </script>
 
 <template>
-  <div class="flex flex-col w-max box-border cursor-pointer" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
+  <div class="flex flex-col max-w-[200px] h-full box-border cursor-pointer" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseOut">
     <div :class="!detect ? 'dark' : 'light'">
-      <img class="w-full" :src="imgUrl">
+      <img :src="imgUrl">
       <div class="inner-frame">
         <text-sub2>{{ text }}</text-sub2>
       </div>
@@ -40,13 +40,13 @@ const handleClick = () => {
 
 <style scoped>
 .light {
-    @apply w-max p-2 max-w-[200px] pb-0;
+    @apply h-full p-2 pb-0;
     filter: drop-shadow(0px 0px 5px #FF3333);
     -webkit-filter: drop-shadow(0px 0px 5px #FF3333);
     background: linear-gradient(180deg, #DF322A 0%, #C0261F 100%);
 }
 .dark {
-    @apply w-max p-2 max-w-[200px] pb-0;
+    @apply h-full p-2 pb-0;
     background: linear-gradient(148.99deg, #770000 0%, #C80000 34.94%, #770000 69.5%);
 }
 
