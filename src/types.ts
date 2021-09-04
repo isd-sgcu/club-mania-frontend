53cff1ad990d65ae087fd3e9b26272100cb384fd
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 00:52:53 +0700

    🏷️ define type for club static info

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,16 @@ import { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
 
-// IMPORTANT some colors in Figma are named 'Vishagarn' not 'Vichagarn', so watchout if that's the case
+// IMPORTANT some colors in Figma are named 'Vishagarn' not 'Vichagarn' so watchout if that's the case
 export type ThemeOption = 'SilpVat' | 'Vichagarn' | 'Gera' | 'Pat' | 'Other' | 'Main'
 
 export type RouteCategoryOption = 'geela' | 'pat' | 'other' | 'slipvat' | 'wichakarn'
+
+export type ClubStaticInfo = {
+  name: string
+  category: string
+  about: string
+  whatToExpect: string
+  recruitmentPeriod: string
+  contact: string
+}
