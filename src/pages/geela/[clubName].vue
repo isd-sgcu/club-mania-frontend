a58451362b004a7523dba9982692e0843f2c2a44
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 01:34:46 +0700

    🚧 category name is needed to load file dynamically

--- src/pages/geela/[clubName].vue
+++ src/pages/geela/[clubName].vue
@@ -1,13 +1,15 @@
 <template>
-  <ClubPage :club-name="props.clubName" />
+  <ClubPage :club-name="props.clubName" :category="category" />
 </template>
 
 <script setup lang="ts">
 import { goToCategoryIfNotValid } from '~/components/pages/club/validateRoute'
+import { RouteCategoryOption } from '~/types'
 
 const props = defineProps<{
   clubName: string
 }>()
 
-goToCategoryIfNotValid(props.clubName, 'geela')
+const category: RouteCategoryOption = 'geela'
+goToCategoryIfNotValid(props.clubName, category)
 </script>
