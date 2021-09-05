2f350d92337e7b967fbb245df4a29143d2e09282
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 21:57:07 +0700

    🚧 update geela clubs info by hand

--- src/assets/clubs/geela/chulalongkorn-university-dancesport-club.js
+++ src/assets/clubs/geela/chulalongkorn-university-dancesport-club.js
@@ -1,10 +1,10 @@
 export const info = {
-  "name": "CU Dancesport Club (ชมรมลีลาศ)",
-  "category": "ฝ่ายกีฬา",
-  "about": "-",
-  "whatToExpect": "ได้ความรู้ความเข้าใจเกี่ยวกับกีฬาลีลาศว่าจริงๆแล้วกีฬาลีลาศไม่จำกัดเพศและไม่จำกัดอายุขอให้คุณรักในการเต้นคุณก็จะได้สนุกกับการเต้น",
-  "recruitmentPeriod": "เปิดรับตบอดเลยครับ",
-  "contact": "IG: cudancesportclubFacebook: Chulalongkorn University Dancesport Club",
-  "badge": "Dancesport Club",
-  "images": []
+  name: 'CU Dancesport Club',
+  category: 'ฝ่ายกีฬา',
+  about: 'CU Dancesport Club (ชมรมลีลาศ)',
+  whatToExpect: 'ได้ความรู้ความเข้าใจเกี่ยวกับกีฬาลีลาศว่าจริงๆแล้วกีฬาลีลาศไม่จำกัดเพศและไม่จำกัดอายุขอให้คุณรักในการเต้นคุณก็จะได้สนุกกับการเต้น',
+  recruitmentPeriod: 'เปิดรับตลอดเลยครับ',
+  contact: 'IG: cudancesportclub\nFacebook: Chulalongkorn University Dancesport Club',
+  badge: 'Dancesport',
+  images: [],
 }
--- src/assets/clubs/geela/cu-fencing-club.js
+++ src/assets/clubs/geela/cu-fencing-club.js
@@ -1,10 +1,10 @@
 export const info = {
-  "name": "ชมรมฟันดาบสากล ",
-  "category": "ฝ่ายกีฬา",
-  "about": "เป็นชมรมกีฬา และเป็นตัวแทนนักกีฬาฟันดาบของจุฬาลงกรณ์มหาวิทยาลัยในการแข่งขันรายการต่างๆ",
-  "whatToExpect": "ได้สุขภาพที่แข็งแรงและมิตรภาพดีๆ",
-  "recruitmentPeriod": "เปิดรับสมัครตลอดเวลาเลย",
-  "contact": "Ig: chula_fencing facebook: cu fencing club",
-  "badge": "CU Fencing Club",
-  "images": []
+  name: 'ชมรมฟันดาบสากล',
+  category: 'ฝ่ายกีฬา',
+  about: 'เป็นชมรมกีฬา และเป็นตัวแทนนักกีฬาฟันดาบของจุฬาลงกรณ์มหาวิทยาลัยในการแข่งขันรายการต่าง ๆ',
+  whatToExpect: 'ได้สุขภาพที่แข็งแรงและมิตรภาพดี ๆ',
+  recruitmentPeriod: 'เปิดรับสมัครตลอดเวลาเลย',
+  contact: 'Ig: chula_fencing \nfacebook: cu fencing club',
+  badge: 'CU Fencing Club',
+  images: [],
 }
