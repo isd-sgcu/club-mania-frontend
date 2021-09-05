31ba5c36e3b9f09fe8acd430458cdecc41e040df
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 20:47:59 +0700

    🚧 updated registered routes

--- src/components/pages/club/routes.ts
+++ src/components/pages/club/routes.ts
@@ -1,15 +1,50 @@
-export const geelaRoutes = new Set<string>([
-  'tennis',
-])
-export const silpvatRoutes = new Set<string>([
-  '',
+export const otherRoutes = new Set<string>([
+  'cu-leadership-club',
+  'co-founder-trainee',
+  'cu-blood',
+  'cu-mun',
+  'charm-chula',
+  'coach-by-champ'
 ])
 export const wichakarnRoutes = new Set<string>([
-  '',
-])
-export const otherRoutes = new Set<string>([
-  '',
+  'potential-club',
+  'cu-cathoric',
+  'chulalongkorn-university-buddhist-society-cubs',
+  'cu-pen-club',
+  'eq-club',
+  'cuigen',
+  'ceo',
+  'cu-gavel-club'
 ])
 export const patRoutes = new Set<string>([
-  '',
+  'anurak-chula',
+  'lohit-club-chula',
+  'hill-tribe-club',
+  'slum-club',
+  'chula-volunteers-for-vulnerable-children-club',
+  'voluntary-club-of-the-student-union-chulalongkorn-university',
+  'aiesec-in-chulalongkorn-university'
+])
+export const slipvatRoutes = new Set<string>([
+  'awutthai',
+  'cu-chorus',
+  'cu-band',
+  'cu-thai-classical-music-club',
+  'mvcu',
+  'cu-photo-club',
+  'isan-art-and-culture-club',
+  'cu-southern-art-and-culture-club',
+  'thai-classical-dance-and-drama-club-chulalongkorn-university',
+  'cudc',
+  'cu-northern-art-and-culture-club'
+])
+export const geelaRoutes = new Set<string>([
+  'chula-esports',
+  'petanquechula',
+  'cu-fencing-club',
+  'chulalongkorn-university-dancesport-club',
+  'chula-rowing-crew',
+  'cu-judo-club',
+  'chula-shooting-club',
+  'chulalongkorn-futsal-team'
 ])
