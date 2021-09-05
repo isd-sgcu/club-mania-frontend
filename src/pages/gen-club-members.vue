a0b3279e20c455bb511e07c5553be2b59fd8deb4
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-05 11:39:39 +0700

    🚧 this should be on this branch

---
+++ src/pages/gen-club-members.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="bg-red mt-30" @click="gen">
+    <h1 class="text-white">
+      gen docs for clubMembers collection
+    </h1>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { doc, setDoc } from 'firebase/firestore'
+import clubMembers from '~/assets/clubs/club_members'
+import { db } from '~/firebase'
+
+// * generate personal info docs
+const gen = () => {
+  for (const clubName of Object.keys(clubMembers)) {
+    const data = clubMembers[clubName]
+    const clubRef = doc(db.value, 'clubMembers', clubName)
+    setDoc(clubRef, data)
+  }
+}
+</script>
