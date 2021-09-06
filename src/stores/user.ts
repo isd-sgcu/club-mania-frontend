f195cf2c1873f17ee88a7f2070861d7253240200
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-06 07:23:34 +0700

    🚧 More apis to the user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,18 +1,48 @@
+import { doc, DocumentReference } from 'firebase/firestore'
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import { db } from '~/firebase'
 
 export const useUserStore = defineStore('user', () => {
-  const clubOfUser = ref<string | null>(null) // club route
+  const clubOfUser = ref<DocumentReference | null>(null) // club route
   const displayName = ref<string | null>(null)
   const badge = ref<string | null>(null)
 
-  const setBadge = (str: string) => {
-    badge.value = str
+  const reset = () => {
+    clubOfUser.value = displayName.value = badge.value = null
+  }
+
+  /**
+   * Checks if the user is a member of the specified club name
+   * @param clubName Club name as in route
+   * @returns True if this member is a member of the specified club
+   */
+  const isMember = (clubName: string) => {
+    if (clubOfUser.value === null) return false
+    const currentClubRef = doc(db.value!, 'clubs', clubName)
+    return clubOfUser.value === currentClubRef
+  }
+
+  /**
+   * Sets the badge of a club member to the user store
+   * @param badgeText the badge of this club
+   */
+  const setBadge = (badgeText: string) => {
+    badge.value = badgeText
   }
   const setDisplayName = (name: string) => {
     displayName.value = name
   }
-  const setClubOfUser = (clubRoute: string) => {
-    clubOfUser.value = clubRoute
+  const setClubOfUser = (clubRef: DocumentReference) => {
+    clubOfUser.value = clubRef
+  }
+
+  const setMemberValues = async({ nickname, clubRef }: {
+    nickname: string
+    clubRef: DocumentReference
+  }) => {
+    // badge is needed to set separately because it lives in the static info files
+    displayName.value = nickname
+    clubOfUser.value = clubRef
   }
 
   return {
@@ -22,6 +52,9 @@ export const useUserStore = defineStore('user', () => {
     setBadge,
     setDisplayName,
     setClubOfUser,
+    reset,
+    setMemberValues,
+    isMember,
   }
 })
 
