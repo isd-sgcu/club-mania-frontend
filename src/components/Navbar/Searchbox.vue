eab83eb90be70689d6d69a3d0502fb74b236ca07
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-06 16:12:46 +0700

    🎨 Make searchbox size to be more consistent

--- src/components/Navbar/Searchbox.vue
+++ src/components/Navbar/Searchbox.vue
@@ -3,15 +3,15 @@
     :to="embedLink"
     class="cursor-pointer hover:bg-grey-primary px-6 my-3 md:(flex max-h-120px bg-white p-4 my-0 hover:bg-grey-light)"
   >
-    <img :src="image" class="w-18 h-auto mr-4 lg:(w-22 h-auto mr-8) <md:hidden" />
-    <div class="md:(flex flex-col py-4)">
-      <p class="font-Mitr text-white font-300 text-[10px] leading-[16px] md:hidden">
+    <img :src="image" class="min-w-18 w-18 h-auto lg:(w-22 h-auto) <md:hidden" />
+    <div class="px-3 md:(flex flex-col justify-center)">
+      <p class="font-Mitr max-w-44 text-white font-300 text-[10px] leading-[16px] md:hidden">
         {{ name }}
       </p>
-      <text-sub1 class="text-purple-700 <md:hidden <lg:text-[18px]">
+      <text-sub2 class="text-purple-700 max-w-65 overflow-clip <md:hidden <lg:text-[18px]">
         {{ name }}
-      </text-sub1>
-      <text-body2 class="<md:hidden <lg:text-[10px]">
+      </text-sub2>
+      <text-body2 class=" <md:hidden <lg:text-[10px]">
         {{ description }}
       </text-body2>
     </div>
