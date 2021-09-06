49457c00fab0ed488a2603268392024afac6ad9e
miello <partearth1212@gmail.com>
2021-09-06 15:26:31 +0700

    📱 Make gallery responsive for 2 images

--- src/components/Frame/PictureFrame/Gallery.vue
+++ src/components/Frame/PictureFrame/Gallery.vue
@@ -5,7 +5,10 @@
         <img class="h-lg cover-center" :src="images[0]" :alt="clubName" />
       </div>
     </div>
-    <div v-else-if="props.images.length === 2"></div>
+    <div v-else-if="props.images.length === 2" class="w-full max-w-[900px] grid grid-cols-2 grid-rows-1 gap-3">
+      <img class="w-full rounded-lg cover-center" :src="images[0]" :alt="clubName" />
+      <img class="w-full rounded-lg cover-center" :src="images[1]" :alt="clubName" />
+    </div>
     <div v-else-if="props.images.length === 3" class="flex flex-col h-auto  w-full sm:(h-md flex-row) lg:(h-lg) xl:(h-xl)">
       <img class="<sm:(w-full) rounded-lg w-2/3  mr-2 cover-center" :src="images[0]" :alt="clubName" />
       <div class="flex <sm:(flex-row) flex-col space-y-2 overflow-hidden rounded-lg">
@@ -21,7 +24,7 @@
         />
       </div>
     </div>
-    <div v-else-if="props.images.length >= 4 " class="flex flex-col h-auto max-w-[600px] w-full grid grid-cols-2 grid-rows-2 gap-3">
+    <div v-else-if="props.images.length >= 4 " class="h-auto max-w-[900px] w-full grid grid-cols-2 grid-rows-2 gap-3">
       <img class="w-full rounded-lg cover-center" :src="images[0]" :alt="clubName" />
       <img class="w-full rounded-lg cover-center" :src="images[1]" :alt="clubName" />
       <img class="w-full rounded-lg cover-center" :src="images[2]" :alt="clubName" />
