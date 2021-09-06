9eb36d9baa86d30527660b21d5619d103ac51f4a
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-06 07:22:41 +0700

    ✨Can now check if user is a member of a club

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
+import { collection, doc, getDoc } from 'firebase/firestore'
+import { auth, db } from './firebase'
+import { CollectionOption, MemberDoc } from './firestore'
+import { useUserStore } from './stores/user'
 import { LocalStorageKeys } from './types'
+
+export const getCollectionRef = (collectionName: CollectionOption) => {
+  return collection(db.value!, collectionName)
+}
 
 export const getFromLocal = (key: LocalStorageKeys) => {
   return localStorage.getItem(key)
@@ -28,4 +36,40 @@ export const getAnonymousId = () => {
   const newId = guid()
   setToLocal('anonymousId', newId)
   return newId
+}
+
+/**
+ * checks if is a member of a club if yes set nickname, year to the user store else reset values
+ * @note This does not set the badge
+ */
+export const setValuesIfIsMember = async() => {
+  // checks if is a member of a club
+  // if yes set nickname, year to the user store, *not the badge
+  // else reset values
+
+  const store = useUserStore()
+  const user = auth.value!.currentUser
+
+  // user is not logged in
+  if (!user) {
+    store.reset()
+    return
+  }
+
+  // checks if user is a member of a club
+  // const membersCollectionRef = getCollectionRef('members')
+  // const q = query(membersCollectionRef, where(user.email,))
+  const userRef = doc(db.value!, 'members', user.email!)
+  const userSnap = await getDoc(userRef)
+
+  if (!userSnap.exists()) {
+    store.reset()
+    return
+  }
+
+  const userDoc = userSnap.data() as MemberDoc
+  store.setMemberValues({
+    nickname: userDoc.name,
+    clubRef: userDoc.club,
+  })
 }
