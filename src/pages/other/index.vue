a6f92d4e90fc402a27174573f98ca8d33910521d
miello <partearth1212@gmail.com>
2021-09-06 18:14:44 +0700

    💄 Apply color to pat and other category

--- src/pages/other/index.vue
+++ src/pages/other/index.vue
@@ -4,12 +4,16 @@
 
 <script setup lang="ts">
 import { clubList } from '~/assets/clubs/clubList'
+import { GhostColorType } from '~/components/Card/Other/types'
 
-const data = clubList.filter(value => value.category === 'other').map((value) => {
+const OtherColor = ['lightcyan', 'pink', 'red', 'steel', 'yellow'] as GhostColorType[]
+
+const data = clubList.filter(value => value.category === 'other').map((value, index) => {
   return {
     text: value.name,
     imgUrl: value.logo,
     href: value.url,
+    colorOther: OtherColor[index % 5],
   }
 })
 
--- src/pages/pat/index.vue
+++ src/pages/pat/index.vue
@@ -4,12 +4,16 @@
 
 <script setup lang="ts">
 import { clubList } from '~/assets/clubs/clubList'
+import { PaletteKey } from '~/components/Card/Pat/types'
 
-const data = clubList.filter(value => value.category === 'pat').map((value) => {
+const color = ['red', 'blue', 'yellow', 'green'] as PaletteKey[]
+
+const data = clubList.filter(value => value.category === 'pat').map((value, index) => {
   return {
     text: value.name,
     imgUrl: value.logo,
     href: value.url,
+    colorPat: color[index % 4],
   }
 })
 
