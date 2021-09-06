c7ddea81e1d3e2ecb7e4d6ac8bffc03908964be4
miello <partearth1212@gmail.com>
2021-09-06 18:02:51 +0700

    ✨ Filter data for each category

--- src/pages/geela/index.vue
+++ src/pages/geela/index.vue
@@ -3,5 +3,12 @@
 </template>
 
 <script setup lang="ts">
+import { clubList } from '~/assets/clubs/clubList'
+
+const data = clubList.filter(value => value.category === 'geela').map((value) => {
+  return {
+    ...value,
+  }
+})
 
 </script>
