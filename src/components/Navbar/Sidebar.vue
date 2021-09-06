c07847a95bfbcede0025cd0539a0614ab77de81b
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-06 17:36:14 +0700

    🛠 searchTerm cleared on box collasping

--- src/components/Navbar/Sidebar.vue
+++ src/components/Navbar/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="sidebar" :style="transition">
     <!--Navigation menu in mobile screen-->
-    <img v-if="searchTerm === ''" :src="PageIcon['Main']" class="clubmania-icon" />
-    <ol v-if="searchTerm === ''" class="mobile-menu">
+    <img v-if="searchTerm.savedTerm === ''" :src="PageIcon['Main']" class="clubmania-icon" />
+    <ol v-if="searchTerm.savedTerm === ''" class="mobile-menu">
       <router-link to="/wichakarn" @click="$emit('collapse')">
         <li>
           วิชาการ
@@ -24,30 +24,31 @@
         </li>
       </router-link>
     </ol>
-    <text-body2 v-if="searchTerm === ''" class="text-white ml-6 md:hidden">
+    <text-body2 v-if="searchTerm.savedTerm === ''" class="text-white ml-6 md:hidden">
       ค้นหา
     </text-body2>
     <div class="search-form">
       <mdi-magnify class="absolute box-border ml-1 text-[16px] text-[#898989] md:hidden" />
       <input
-        v-model="searchTerm"
+        v-model="searchTerm.savedTerm"
         type="text"
         placeholder="ค้นหาชมรม"
       >
       <carbon-close-filled class="w-32px h-auto ml-8px cursor-pointer hover:text-yellow-700 <md:hidden" @click="$emit('collapse')" />
     </div>
     <!---Search result add here!--->
-    <div v-if="searchTerm !== ''" class="result-container">
+    <div v-if="searchTerm.savedTerm !== ''" class="result-container">
       <searchbox
         v-for="item in foundClubs"
         :key="item.name"
         :embed-link="item.url"
         :name="item.name"
         :description="item.category"
+        @click="$emit('collapse')"
       />
     </div>
     <div
-      v-if="searchTerm === ''"
+      v-if="searchTerm.savedTerm === ''"
       class="absolute w-full bottom-0 flex items-center h-12
       border-[#3D3D3D] border-t-1 text-white pl-6 py-3 md:hidden"
     >
@@ -58,9 +59,7 @@
 </template>
 
 <script setup lang="ts">
-import { clubList } from '~/assets/clubs/clubList'
-
-import { Club } from '~/types'
+import { useSearchTerm } from '~/stores/searchTerm'
 import { PageIcon } from '~/imagePath'
 
 const props = defineProps<{
@@ -71,50 +70,54 @@ const props = defineProps<{
 defineEmits<{
   (e: 'collapse'): void
 }>()
-const MAX_DISPLAY = 6
 
-const searchTerm = ref('')
-const foundClubs = ref<Array<Club>>([])
-const initial = ref(true)
+const searchTerm = useSearchTerm()
+const foundClubs = computed(() => searchTerm.getClubs())
 
-const searchClub = async(searchTerm: string) => {
-  // TODO: implement search logic
-  searchTerm = searchTerm.toLowerCase()
-  foundClubs.value = [] // [[score, club's index]]
-  if (searchTerm !== '') {
-    const bufferArr = []
-    for (let i = 0; i < clubList.length; i++) {
-      const clubname = clubList[i].name.toLowerCase()
-      const idx = clubname.indexOf(searchTerm)
-      if (idx !== -1)
-        bufferArr.push([idx, i])
-    }
+// const MAX_DISPLAY = 6
 
-    const processArr = bufferArr.sort((a, b) => {
-      if (a[0] < b[0])
-        return -1
-      if (a[0] > b[0])
-        return 1
-      return a[1] - b[1]
-    })
-    for (let i = 0; i < MAX_DISPLAY && i < processArr.length; i++) {
-      const idx = processArr[i][1]
-      foundClubs.value.push(clubList[idx])
-    }
-  }
-}
+// const searchTerm = ref('')
+// const foundClubs = ref<Array<Club>>([])
+// const initial = ref(true)
 
-// trigger when there are change in searchTerm
-watch(searchTerm, (searchTerm) => {
-  if (initial.value) {
-    initial.value = false
-    return
-  }
-  const timer = setTimeout(() => {
-    searchClub(searchTerm)
-  }, 500)
-  return () => clearTimeout(timer)
-})
+// const searchClub = async(searchTerm: string) => {
+//   // TODO: implement search logic
+//   searchTerm = searchTerm.toLowerCase()
+//   foundClubs.value = [] // [[score, club's index]]
+//   if (searchTerm !== '') {
+//     const bufferArr = []
+//     for (let i = 0; i < clubList.length; i++) {
+//       const clubname = clubList[i].name.toLowerCase()
+//       const idx = clubname.indexOf(searchTerm)
+//       if (idx !== -1)
+//         bufferArr.push([idx, i])
+//     }
+
+//     const processArr = bufferArr.sort((a, b) => {
+//       if (a[0] < b[0])
+//         return -1
+//       if (a[0] > b[0])
+//         return 1
+//       return a[1] - b[1]
+//     })
+//     for (let i = 0; i < MAX_DISPLAY && i < processArr.length; i++) {
+//       const idx = processArr[i][1]
+//       foundClubs.value.push(clubList[idx])
+//     }
+//   }
+// }
+
+// // trigger when there are change in searchTerm
+// watch(searchTerm, (searchTerm) => {
+//   if (initial.value) {
+//     initial.value = false
+//     return
+//   }
+//   const timer = setTimeout(() => {
+//     searchClub(searchTerm)
+//   }, 500)
+//   return () => clearTimeout(timer)
+// })
 
 const transition = computed(() => props.show ? { right: 0 } : { right: '-100%' })
 </script>
