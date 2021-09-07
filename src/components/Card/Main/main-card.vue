4066f9e841a9227e3b26fc77eb2bfebb6a0667d4
miello <partearth1212@gmail.com>
2021-09-07 18:27:21 +0700

    ✨ Update logo to card

--- src/components/Card/Main/main-card.vue
+++ src/components/Card/Main/main-card.vue
@@ -7,7 +7,7 @@
     @mouseleave="handleMouseOut"
   >
     <div class="p-3 rounded-tl-md rounded-tr-md" style="background: linear-gradient(160.98deg, #130000 0%, #7B7B7B 34.44%, #130000 68.49%);">
-      <img :src="imgUrl || 'https://dummyimage.com/200x200/5c205c/ffffff.png'">
+      <img class="bg-white" :src="imgUrl || 'https://dummyimage.com/200x200/5c205c/ffffff.png'">
     </div>
     <div class="flex justify-center items-center flex-1 break-words text-white p-4 pt-2 text-center rounded-bl-md rounded-br-md" :style="{ background: CardBackground + 'linear-gradient(138.79deg, #130000 70%, #4F4F4F 85%, #130000 100%)' }">
       <text-sub2>{{ text }}</text-sub2>
@@ -32,6 +32,8 @@ const props = defineProps<IProps>()
 const CardBackground = ref('')
 const CardBorder = CardVariant.border[props.type || 'Other']
 const borderWidth = ref('3px')
+
+console.log(props.imgUrl)
 
 const handleMouseEnter = () => {
   borderWidth.value = '5px'
