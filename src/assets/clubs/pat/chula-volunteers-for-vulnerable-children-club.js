28a718a9ee0fac2a88ae2f463de8c0deb33eb73f
miello <partearth1212@gmail.com>
2021-09-07 22:45:43 +0700

    🍱 Add link for image url for each clubs

--- src/assets/clubs/pat/chula-volunteers-for-vulnerable-children-club.js
+++ src/assets/clubs/pat/chula-volunteers-for-vulnerable-children-club.js
@@ -1,10 +1,18 @@
 export const info = {
   name: 'ชมรมอาสาพัฒนาเพื่อเด็กด้อยโอกาส',
   category: 'ฝ่ายพัฒนาสังคมและบำเพ็ญประโยชน์',
-  about: 'ชมรมอาสาพัฒนาเพื่อเด็กด้อยโอกาสแห่งจุฬาลงกรณ์มหาวิทยาลัย (cvvc)\nเป็นชมรมอาสาที่เสริมสร้างความเข้มแข็งทางด้านจิตใจให้กับเด็กด้อยโอกาส ผ่านหลักความเข้มแข็งทางด้านจิตใจ 4 Dimensions เราเน้นเรื่องการสร้างแรงบันดาลใจ สร้างแรงผลักดัน และมอบโอกาสที่เพิ่มขึ้นให้กับเด็กๆ เพื่อให้เด็กมีความเข้มแข็งทางด้านจิตใจ มีความคิดที่ดี มีเป้าหมายและแรงผลักดันในการใช้ชีวิต ตลอดจนสามารถเติบโตเป็นเด็กที่มีคุณภาพต่อได้',
-  whatToExpect: 'ความสนุกสนาน สนิทสนม และมิตรภาพดีๆจากเพื่อนๆในชมรม ร่วมออกค่ายช่วยเหลือและเสริมสร้างเด็กด้อยโอกาส การอาสาช่วยเหลือโรงเรียน ความรัก ความปลาบปลื้มใจ และความตื้นตันจากเด็กมากมาย กิจกรรมสนุกสนานตลอดค่าย และทริปกระชับสัมพันธ์หลังค่าย',
+  about:
+    'ชมรมอาสาพัฒนาเพื่อเด็กด้อยโอกาสแห่งจุฬาลงกรณ์มหาวิทยาลัย (cvvc)\nเป็นชมรมอาสาที่เสริมสร้างความเข้มแข็งทางด้านจิตใจให้กับเด็กด้อยโอกาส ผ่านหลักความเข้มแข็งทางด้านจิตใจ 4 Dimensions เราเน้นเรื่องการสร้างแรงบันดาลใจ สร้างแรงผลักดัน และมอบโอกาสที่เพิ่มขึ้นให้กับเด็กๆ เพื่อให้เด็กมีความเข้มแข็งทางด้านจิตใจ มีความคิดที่ดี มีเป้าหมายและแรงผลักดันในการใช้ชีวิต ตลอดจนสามารถเติบโตเป็นเด็กที่มีคุณภาพต่อได้',
+  whatToExpect:
+    'ความสนุกสนาน สนิทสนม และมิตรภาพดีๆจากเพื่อนๆในชมรม ร่วมออกค่ายช่วยเหลือและเสริมสร้างเด็กด้อยโอกาส การอาสาช่วยเหลือโรงเรียน ความรัก ความปลาบปลื้มใจ และความตื้นตันจากเด็กมากมาย กิจกรรมสนุกสนานตลอดค่าย และทริปกระชับสัมพันธ์หลังค่าย',
   recruitmentPeriod: 'เปิดรับสมัครตั้งแต่วันที่ 9 - 30 กันยายน 2564',
-  contact: 'Facebook : chula volunteers for vulnerable children club: cvvc\nInstagram : cvvc.official',
+  contact:
+    'Facebook : chula volunteers for vulnerable children club: cvvc\nInstagram : cvvc.official',
   badge: 'อาสาพัฒนาเด็ก',
-  images: [],
+  images: [
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fchula-volunteers-for-vulnerable-children-club%2F.DS_Store?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fchula-volunteers-for-vulnerable-children-club%2F1.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fchula-volunteers-for-vulnerable-children-club%2F2.JPG?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fchula-volunteers-for-vulnerable-children-club%2F3.jpg?alt=media',
+  ],
 }
--- src/assets/clubs/pat/hill-tribe-club.js
+++ src/assets/clubs/pat/hill-tribe-club.js
@@ -1,10 +1,19 @@
 export const info = {
   name: 'ชมรมค่ายอาสาพัฒนาชาวไทยภูเขา',
   category: 'ฝ่ายพัฒนาสังคมและบำเพ็ญประโยชน์',
-  about: 'ชมรมค่ายอาสาพัฒนาชาวไทยภูเขา จุฬาลงกรณ์มหาวิทยาลัย (HTC)\nชมรมของเรามีจุดประสงค์หลักคือการเรียนรู้วิถีชีวิตและพัฒนาความเป็นอยู่ของชาวไทยภูเขาชาติพันธุ์ต่างๆในไทยผ่านกิจกรรมค่ายอาสา และการใช้ social media เป็นกระบอกเสียงให้แก่ชาวไทยภูเขาในด้านต่างๆ',
-  whatToExpect: 'น้องๆจะได้เรียนรู้แง่มุมต่างๆของวิถีชีวิตชาวไทยภูเขา ได้เปิดมุมมองให้กว้างขึ้น ได้ประสบการณ์ที่มีค่าจากการทำงาน ได้พบเจอเพื่อนใหม่ๆ ได้สร้างความทรงจำดีๆ และที่สำคัญคือได้เป็นส่วนหนึ่งของครอบครัวแสนอบอุ่นของเรา!',
+  about:
+    'ชมรมค่ายอาสาพัฒนาชาวไทยภูเขา จุฬาลงกรณ์มหาวิทยาลัย (HTC)\nชมรมของเรามีจุดประสงค์หลักคือการเรียนรู้วิถีชีวิตและพัฒนาความเป็นอยู่ของชาวไทยภูเขาชาติพันธุ์ต่างๆในไทยผ่านกิจกรรมค่ายอาสา และการใช้ social media เป็นกระบอกเสียงให้แก่ชาวไทยภูเขาในด้านต่างๆ',
+  whatToExpect:
+    'น้องๆจะได้เรียนรู้แง่มุมต่างๆของวิถีชีวิตชาวไทยภูเขา ได้เปิดมุมมองให้กว้างขึ้น ได้ประสบการณ์ที่มีค่าจากการทำงาน ได้พบเจอเพื่อนใหม่ๆ ได้สร้างความทรงจำดีๆ และที่สำคัญคือได้เป็นส่วนหนึ่งของครอบครัวแสนอบอุ่นของเรา!',
   recruitmentPeriod: 'รอติดตามข้อมูลได้ใน Facebook ของชมรมเลยยย',
-  contact: 'Facebook: Hill Tribe Club CU - ชมรมค่ายอาสาพัฒนาชาวไทยภูเขา จุฬาลงกรณ์มหาวิทยาลัย\nIG: @htc.chula Twitter: @htcChula',
+  contact:
+    'Facebook: Hill Tribe Club CU - ชมรมค่ายอาสาพัฒนาชาวไทยภูเขา จุฬาลงกรณ์มหาวิทยาลัย\nIG: @htc.chula Twitter: @htcChula',
   badge: 'Hill Tribe Club',
-  images: [],
+  images: [
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fhill-tribe-club%2F1.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fhill-tribe-club%2F2.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fhill-tribe-club%2F3.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fhill-tribe-club%2F4.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/pat%2Fhill-tribe-club%2F5.jpg?alt=media',
+  ],
 }
