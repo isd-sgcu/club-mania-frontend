75517ce2d104e46d851c4a16f71fb3d755f4d553
miello <partearth1212@gmail.com>
2021-09-07 23:08:39 +0700

    🐛 Fix wrong url image

--- src/assets/clubs/other/cu-leadership-club.js
+++ src/assets/clubs/other/cu-leadership-club.js
@@ -7,9 +7,9 @@ export const info = {
   contact: 'FB: CU  Leadership Club\nIG: culeadershipclub',
   badge: 'CU Leadership Club',
   images: [
-    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-blood%2F1.jpg?alt=media',
-    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-blood%2F2.jpg?alt=media',
-    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-blood%2F3.jpg?alt=media',
-    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-blood%2F5.jpg?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-leadership-club%2F1.JPG?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-leadership-club%2F2.JPG?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-leadership-club%2F3.JPG?alt=media',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/other%2Fcu-leadership-club%2F5.JPG?alt=media',
   ],
 }
