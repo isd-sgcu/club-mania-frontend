2b34cd2a0ee1a9e93bae8e6a3595c6229714909a
miello <partearth1212@gmail.com>
2021-09-08 18:25:23 +0700

    ✨ Now gallery can specific by clicking thumbnail

--- src/components/Frame/PictureFrame/Gallery.vue
+++ src/components/Frame/PictureFrame/Gallery.vue
@@ -2,34 +2,36 @@
   <div class="w-full flex justify-center">
     <div v-if="props.images.length === 1">
       <div class="rounded-lg">
-        <img class="h-lg cover-center" :src="images[0]" :alt="clubName" />
+        <img class="h-lg cover-center cursor-pointer" :src="images[0]" :alt="clubName" @click="() => handleOpenGallery(1)" />
       </div>
     </div>
     <div v-else-if="props.images.length === 2" class="w-full grid grid-cols-2 grid-rows-1 gap-3">
-      <img class="w-full h-full rounded-lg cover-center" :src="images[0]" :alt="clubName" />
-      <img class="w-full h-full rounded-lg cover-center" :src="images[1]" :alt="clubName" />
+      <img class="w-full h-full rounded-lg cover-center cursor-pointer" :src="images[0]" :alt="clubName" @click="() => handleOpenGallery(1)" />
+      <img class="w-full h-full rounded-lg cover-center cursor-pointer" :src="images[1]" :alt="clubName" @click="() => handleOpenGallery(2)" />
     </div>
     <div v-else-if="props.images.length === 3" class="flex flex-col h-auto w-full sm:(h-md flex-row) lg:(h-lg) xl:(h-xl)">
-      <img class="<sm:(w-full) rounded-lg w-2/3  mr-2 cover-center" :src="images[0]" :alt="clubName" />
+      <img class="<sm:(w-full) rounded-lg w-2/3  mr-2 cover-center cursor-pointer" :src="images[0]" :alt="clubName" @click="() => handleOpenGallery(1)" />
       <div class="flex <sm:(flex-row) flex-col space-y-2 overflow-hidden rounded-lg">
         <img
-          class="<sm:(h-full w-2/3 mr-2 mt-2) rounded-lg h-1/2  cover-center"
+          class="<sm:(h-full w-2/3 mr-2 mt-2) rounded-lg h-1/2  cover-center cursor-pointer"
           :src="images[1]"
           :alt="clubName"
+          @click="() => handleOpenGallery(2)"
         />
         <img
-          class="<sm:(h-full w-1/3) rounded-lg h-1/2 cover-center"
+          class="<sm:(h-full w-1/3) rounded-lg h-1/2 cover-center cursor-pointer"
           :src="images[2]"
           :alt="clubName"
+          @click="() => handleOpenGallery(3)"
         />
       </div>
     </div>
     <div v-else-if="props.images.length >= 4 " class="h-auto w-full grid grid-cols-2 grid-rows-2 gap-3">
-      <img class="w-full rounded-lg cover-center h-full" :style="{ aspectRatio: '4/3'}" :src="images[0]" :alt="clubName" />
-      <img class="w-full rounded-lg cover-center h-full" :style="{ aspectRatio: '4/3'}" :src="images[1]" :alt="clubName" />
-      <img class="w-full rounded-lg cover-center h-full" :style="{ aspectRatio: '4/3'}" :src="images[2]" :alt="clubName" />
+      <img class="w-full rounded-lg cover-center h-full cursor-pointer" :style="{ aspectRatio: '4/3'}" :src="images[0]" :alt="clubName" @click="() => handleOpenGallery(1)" />
+      <img class="w-full rounded-lg cover-center h-full cursor-pointer" :style="{ aspectRatio: '4/3'}" :src="images[1]" :alt="clubName" @click="() => handleOpenGallery(2)" />
+      <img class="w-full rounded-lg cover-center h-full cursor-pointer" :style="{ aspectRatio: '4/3'}" :src="images[2]" :alt="clubName" @click="() => handleOpenGallery(3)" />
       <div class="relative">
-        <div v-if="props.images.length !== 4" class="flex flex-row absolute w-full h-full more-detail justify-center items-center">
+        <div v-if="props.images.length !== 4" class="flex flex-row absolute w-full h-full more-detail justify-center items-center cursor-pointer" @click="() => handleOpenGallery(4)">
           <IcOutlinePlus color="white" font-size="4rem" />
           <TextSub1 class="text-white !text-[4rem]">
             {{ props.images.length - 3 }}
@@ -45,6 +47,7 @@
 const props = defineProps<{
   images: string[]
   clubName: string // use as alt if alt is not specified
+  handleOpenGallery(newValue: number): void
 }>()
 </script>
 
