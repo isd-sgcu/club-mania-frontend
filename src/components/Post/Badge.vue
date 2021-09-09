82b3c1e62a71b55a5f4da8f89441e07c81f129be
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-09 18:42:24 +0700

    💄 make "Staff" uppercase just to feel privileged

--- src/components/Post/Badge.vue
+++ src/components/Post/Badge.vue
@@ -1,7 +1,7 @@
 <template>
   <span
     class="<sm:(px-2 text-sm) italic border-1 rounded-full px-3 py-0.5 text-center"
-    :class="`border-${badgeBorder[decidedTheme]} text-${badgeTextColor[decidedTheme]}`"
+    :class="[`border-${badgeBorder[decidedTheme]} text-${badgeTextColor[decidedTheme]}`, { uppercase: isStaff }]"
   >{{ badgeText }}</span>
 </template>
 
@@ -13,6 +13,7 @@ const themeStore = useThemeStore()
 const props = defineProps<{ badgeText: string; theme?: ThemeOption }>()
 
 const decidedTheme = props.theme ?? themeStore.savedTheme
+const isStaff = props.badgeText.toLowerCase() === 'staff'
 
 const badgeBorder = {
   SilpVat: 'blue-b100',
