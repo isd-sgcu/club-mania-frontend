ba888fb16fc7b39b951de3ef45c4f5412d528531
miello <partearth1212@gmail.com>
2021-09-10 00:47:15 +0700

    🐛 Fix modal by making it stick to screen

--- src/components/Card/Other/other-card.vue
+++ src/components/Card/Other/other-card.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="relative">
       <img :src="!detect ? backgroundImg : backgroundHover" class="w-full h-full" preserveAspectRatio="none">
-      <text-sub2 class="w-[200px] <sm:(text-size-[0.7rem] leading-5) max-w-[180px] absolute break-words pl-6 pr-6 pb-2 text-center text-card" :class="`text-${!detect ? textColor : 'white'}`">
+      <text-sub2 class="w-[200px] <sm:(text-size-[0.7rem] leading-5) <lg:(text-size-[0.8rem]) max-w-[180px] absolute break-words pl-6 pr-6 pb-2 text-center text-card" :class="`text-${!detect ? textColor : 'white'}`">
         {{ text }}
       </text-sub2>
     </div>
@@ -55,13 +55,13 @@ const handleClick = () => {
 <style scoped>
 .img-clip {
     z-index: -1;
-    clip-path: inset(10px 15px 20px 15px);
+    clip-path: inset(10px 17px 20px 18px);
 }
 
 .text-card {
   top: 50%;
   left: 50%;
-  line-height: 1rem;
+  line-height: 1.25rem;
   transform: translate(-50%, -50%);
 }
 </style>
