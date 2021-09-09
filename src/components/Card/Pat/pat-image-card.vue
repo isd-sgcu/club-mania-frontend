e3bc1980d7d3fe5e30883f437cbf0b58381a9882
miello <partearth1212@gmail.com>
2021-09-10 00:21:33 +0700

    🐛 Fix height of pat card that incorrect in safari

--- src/components/Card/Pat/pat-image-card.vue
+++ src/components/Card/Pat/pat-image-card.vue
@@ -12,9 +12,11 @@ defineProps<IProps>()
 </script>
 
 <template>
-  <PatBorderCard :color="color">
+  <PatBorderCard class="h-[200px]" :color="color">
     <div class="p-2 h-full">
-      <img class="bg-white box-border h-full object-contain w-full py-2" :src="imgUrl">
+      <div class="flex h-full">
+        <img class="bg-white box-border object-contain w-full py-2" :src="imgUrl">
+      </div>
     </div>
   </PatBorderCard>
 </template>
