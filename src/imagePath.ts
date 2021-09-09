947a63f4e72ec0e182fa6531dfc64dff3fb18c34
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-10 01:50:26 +0700

    🤡 Move static image files to public folder

--- src/imagePath.ts
+++ src/imagePath.ts
@@ -1,18 +1,25 @@
-import MainBanner from '~/assets/Banners/banner-Main.png'
-import VichagarnBanner from '~/assets/Banners/banner-Vichagarn.png'
-import GeraBanner from '~/assets/Banners/banner-Gera.png'
-import SlipVatBanner from '~/assets/Banners/banner-SilpVat.png'
-import PatBanner from '~/assets/Banners/banner-Pat.png'
-import OtherBanner from '~/assets/Banners/banner-Other.png'
+import MainBanner from '/Banners/banner-Main.png'
+import VichagarnBanner from '/Banners/banner-Vichagarn.png'
+import GeraBanner from '/Banners/banner-Gera.png'
+import SlipVatBanner from '/Banners/banner-SilpVat.png'
+import PatBanner from '/Banners/banner-Pat.png'
+import OtherBanner from '/Banners/banner-Other.png'
 
-import GeraClubBanner from '~/assets/Banners/banner-club-Gera.png'
-import VichagarnClubBanner from '~/assets/Banners/banner-club-Vichagarn.png'
-import SilpVatClubBanner from '~/assets/Banners/banner-club-SilpVat.png'
-import dummyImage from '~/assets/Banners/No_Image.png'
+import GeraClubBanner from '/Banners/banner-club-Gera.png'
+import VichagarnClubBanner from '/Banners/banner-club-Vichagarn.png'
+import SilpVatClubBanner from '/Banners/banner-club-SilpVat.png'
+import dummyImage from '/Banners/No_Image.png'
 
-import iconWhite from '~/assets/icons/icon_white.png'
-import iconYellow from '~/assets/icons/icon_yellow.png'
-import iconViolet from '~/assets/icons/icon_violet.png'
+import iconWhite from '/icons/icon_white.png'
+import iconYellow from '/icons/icon_yellow.png'
+import iconViolet from '/icons/icon_violet.png'
+
+import MainMobile from '/Banners/mobile-Main.png'
+import VichagarnMobile from '/Banners/mobile-Vichagarn.png'
+import GeraMobile from '/Banners/mobile-Gera.png'
+import SlipVatMobile from '/Banners/mobile-SilpVat.png'
+import PatMobile from '/Banners/mobile-Pat.png'
+import OtherMobile from '/Banners/mobile-Other.png'
 
 export const CategoryBanner = {
   Main: MainBanner,
@@ -30,6 +37,15 @@ export const ClubBanner = {
   SilpVat: SilpVatClubBanner,
   Pat: PatBanner,
   Other: OtherBanner,
+}
+
+export const MobileBanner = {
+  Main: MainMobile,
+  Vichagarn: VichagarnMobile,
+  Gera: GeraMobile,
+  SilpVat: SlipVatMobile,
+  Pat: PatMobile,
+  Other: OtherMobile,
 }
 
 export const PageIcon = {
