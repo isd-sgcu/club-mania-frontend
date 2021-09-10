fb78f34fdd69e492d3aa7de6e1594bfd999cb423
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-10 21:30:26 +0700

    🔍 description in Thai for index page

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { useHead } from '@vueuse/head'
 useHead({
   title: 'Club Mania 2021 | Clubs in Chulalongkorn University',
   meta: [
-    { name: 'description', content: 'Club Mania 2021 is a website that provides information about the clubs within Chulalongkorn University. Visitors can ask qustions about a club directly in each club\'s page' },
-    { name: 'keywords', content: 'club, chula, clubmania, chulalongkorn' },
+    { name: 'description', content: 'Club Mania 2021 เว็บไซต์สำหรับข้อมูลของชมรมต่าง ๆ ในจุฬาลงกรณ์มหาวิทยาลัย ผู้เข้าชมเว็บไซต์สามารถถามคำถามหรือแสดงความคิดเห็นได้ที่หน้าเฉพาะชมรมนั้น ๆ ได้โดยตรง\nClub Mania 2021 is a website that provides information about the clubs within Chulalongkorn University. Visitors can ask qustions about a club directly in each club\'s page' },
+    { name: 'keywords', content: 'club, chula, clubmania, chulalongkorn, ชมรม, จุฬาฯ, จุฬาลงกรณ์มหาวิทยาลัย' },
     { name: 'application-name', content: 'Club Mania 2021' },
   ],
 })
