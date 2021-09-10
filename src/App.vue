05d01af21cb03f11919eb7e8e69a9701f9791b0f
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-10 20:36:44 +0700

    ✏️ typo on meta

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { useHead } from '@vueuse/head'
 useHead({
   title: 'Club Mania 2021 | Clubs in Chulalongkorn University',
   meta: [
-    { name: 'description', content: 'Club Mania 2021 is a website that provides information about the clubs within Chulalongkorn University. Visiters can ask qustions about a club directly in each club\'s page' },
+    { name: 'description', content: 'Club Mania 2021 is a website that provides information about the clubs within Chulalongkorn University. Visitors can ask qustions about a club directly in each club\'s page' },
     { name: 'keywords', content: 'club, chula, clubmania, chulalongkorn' },
     { name: 'application-name', content: 'Club Mania 2021' },
   ],
