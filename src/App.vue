fbc936dcf00c3551c5dc89a7ccf688c37a04fbaa
Arm <69001162+arskiir@users.noreply.github.com>
2021-09-10 20:19:14 +0700

    🔍 add more meta data

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@ import { useHead } from '@vueuse/head'
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Club Mania',
+  title: 'Club Mania 2021 | Clubs in Chulalongkorn University',
   meta: [
-    { name: 'description', content: 'Club Mania description' },
+    { name: 'description', content: 'Club Mania 2021 is a website that provides information about the clubs within Chulalongkorn University. Visiters can ask qustions about a club directly in each club\'s page' },
+    { name: 'keywords', content: 'club, chula, clubmania, chulalongkorn' },
+    { name: 'application-name', content: 'Club Mania 2021' },
   ],
 })
 </script>
