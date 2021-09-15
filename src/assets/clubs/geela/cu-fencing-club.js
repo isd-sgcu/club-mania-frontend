196b5b9f2e1ee398cb269f0d71b98a89657c7c04
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-09-15 20:18:48 +0700

    💄 Update new clubs info

--- src/assets/clubs/geela/cu-fencing-club.js
+++ src/assets/clubs/geela/cu-fencing-club.js
@@ -7,5 +7,11 @@ export const info = {
   recruitmentPeriod: 'เปิดรับสมัครตลอดเวลาเลย',
   contact: 'Ig: chula_fencing \nfacebook: cu fencing club',
   badge: 'CU Fencing Club',
-  images: [],
+  images: [
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/geela%2Fcu-fencing-club%2F1.jpg?alt=media&token=a16f010b-3b82-4a0e-9ae5-bc9855f66602',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/geela%2Fcu-fencing-club%2F2.jpg?alt=media&token=a16f010b-3b82-4a0e-9ae5-bc9855f66602',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/geela%2Fcu-fencing-club%2F3.jpg?alt=media&token=a16f010b-3b82-4a0e-9ae5-bc9855f66602',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/geela%2Fcu-fencing-club%2F4.jpg?alt=media&token=a16f010b-3b82-4a0e-9ae5-bc9855f66602',
+    'https://firebasestorage.googleapis.com/v0/b/club-mania2021.appspot.com/o/geela%2Fcu-fencing-club%2F5.jpg?alt=media&token=a16f010b-3b82-4a0e-9ae5-bc9855f66602',
+  ],
 }
